721f27ff338b6805762a32501fd00e1dcd2d6e74
Yakov Dalinchuk <yakov.dalinchuk@yandex.ru>
2018-09-14 11:18:59 +0300

    build path fix
    (cherry picked from commit db045e2)

--- config/paths.js
+++ config/paths.js
@@ -41,11 +41,7 @@ function getServedPath(appPackageJson) {
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
-
-  // EDIT -->
-  appBuild: resolveApp('../_front_output'),
-  // <-- EDIT
-
+  appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),
