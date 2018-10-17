192ad6e66e138cace3ea0ca9301f8e154b79b3e8
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2018-10-17 17:36:33 +0300

    Cleanup development scripts: deps.sh, start.sh

--- deps.sh
+++ deps.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-if [ -n "$1" ]
-then
-    ROCKS_SERVER=$1
-fi
-
-if [ -z "$ROCKS_SERVER" ]
-then
-    echo "Usage: $0 ROCKS_SERVER"
-    exit 1
-fi
-
-tarantoolctl rocks --server=$ROCKS_SERVER install http 1.0.5
-tarantoolctl rocks --server=$ROCKS_SERVER make
+tarantoolctl rocks install http 1.0.5
+tarantoolctl rocks make
