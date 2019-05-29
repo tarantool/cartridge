a8ac273c05a2a25ee874ac2ba2a397d8cb86f9b9
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-05-29 21:10:53 +0300

    Verify graphql schema is up to date

--- fetch-schema.sh
+++ fetch-schema.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-set -e
-
 WORKDIR=dev/gql-schema test/integration/instance.lua &
 PID=$!
-graphql get-schema -o doc/schema.graphql
+
+TMP=$(mktemp --suffix .graphql)
+graphql get-schema -o $TMP
+
+OUTPUT="doc/schema.graphql"
+diff \
+    --ignore-all-space \
+    --ignore-blank-lines \
+    --ignore-matching-lines "^# timestamp:" \
+    $OUTPUT $TMP
+DIFF=$?
+
+if [ "$DIFF" -eq 0 ]
+then
+    echo "GraphQL schema is up to date!"
+    rm -f $TMP
+else
+    echo "GraphQL schema was updated!"
+    mv -f $TMP $OUTPUT
+fi
+
 kill $PID
+exit $DIFF
