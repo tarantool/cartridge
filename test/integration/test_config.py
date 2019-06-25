0d2cea9d89dbf09109ddd23bce4066800ad13f51
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-06-25 14:04:00 +0300

    Uploading empty config removes all sections

--- test/integration/test_config.py
+++ test/integration/test_config.py
@@ -20,22 +20,21 @@ cluster = [
 
 def test_upload_good(cluster):
     srv = cluster['master']
+    def upload(cfg):
+        srv.put('/admin/config',
+            files={'file': ('config.yaml', yaml.dump(cfg))}
+        )
+
     custom_config = {
-        'custom-config': {
+        'custom_config': {
             'Ultimate Question of Life, the Universe, and Everything': 42
         }
     }
-
-    resp = srv.put('/admin/config',
-        files={'file': (
-            'config.yaml',
-            yaml.dump(custom_config),
-        )}
-    )
+    upload(custom_config)
 
     srv.conn.eval("""
         local confapplier = package.loaded['cluster.confapplier']
-        local custom_config = confapplier.get_readonly('custom-config')
+        local custom_config = confapplier.get_readonly('custom_config')
         local _, answer = next(custom_config)
         assert(answer == 42, 'Answer ~= 42')
     """)
@@ -43,12 +42,37 @@ def test_upload_good(cluster):
     resp = srv.get('/admin/config')
     assert yaml.safe_load(resp) == custom_config
 
+    other_config = {
+        'other_config': {
+            'How many engineers does it take to change a light bulb': 1
+        }
+    }
+    upload(other_config)
+
+    resp = srv.get('/admin/config')
+    conf = yaml.safe_load(resp)
+    assert 'custom_config' not in conf
+    assert 'other_config' in conf
+
+
 def test_upload_fail(cluster):
     srv = cluster['master']
 
-    resp = srv.put_raw('/admin/config', json={'topology': None})
+    resp = srv.put_raw('/admin/config', json={'topology': {}})
     assert resp.status_code == 400
-    assert resp.json()['err'] == 'topology_new must be a table, got nil'
+    assert resp.json()['class_name'] == 'Config upload failed'
+    assert resp.json()['err'] == 'uploading system section "topology" is forbidden'
+
+    resp = srv.put_raw('/admin/config', json={'vshard': {}})
+    assert resp.status_code == 400
+    assert resp.json()['class_name'] == 'Config upload failed'
+    assert resp.json()['err'] == 'uploading system section "vshard" is forbidden'
+
+    resp = srv.put_raw('/admin/config', json={'vshard_groups': {}})
+    assert resp.status_code == 400
+    assert resp.json()['class_name'] == 'Config upload failed'
+    assert resp.json()['err'] == 'uploading system section "vshard_groups" is forbidden'
+
 
     resp = srv.put_raw('/admin/config', files={'file': ','})
     assert resp.status_code == 400
@@ -59,8 +83,3 @@ def test_upload_fail(cluster):
     assert resp.status_code == 400
     assert resp.json()['class_name'] == 'Config upload failed'
     assert resp.json()['err'] == 'Config must be a table'
-
-    resp = srv.put_raw('/admin/config', files={'file': '{ }'})
-    assert resp.status_code == 400
-    assert resp.json()['class_name'] == 'Config upload failed'
-    assert resp.json()['err'] == 'Config must not be empty'
