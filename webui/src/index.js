eb25f648b83e0abe12da33826d6e216c636f0c81
Roman Babaev <r.babaev@corp.mail.ru>
2019-07-18 10:12:29 +0000

    Auth button and form in all modules

--- webui/src/index.js
+++ webui/src/index.js
@@ -3,7 +3,9 @@ import { Provider } from 'react-redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import App from './app';
 import HeaderAuthControl from 'src/components/HeaderAuthControl';
+import LogInForm from 'src/components/LogInForm';
 import configureStore from './store/configureStore';
+import { appDidMount } from 'src/store/actions/app.actions';
 import { logOut } from 'src/store/actions/auth.actions';
 import { PROJECT_NAME } from './constants';
 import { menuReducer } from './menu';
@@ -39,9 +41,13 @@ window.tarantool_enterprise_core.subscribe('cluster:logout', () => {
   store.dispatch(logOut());
 });
 
+store.dispatch(appDidMount());
 
 window.tarantool_enterprise_core.setHeaderComponent(
   <Provider store={store}>
-    <HeaderAuthControl />
+    <React.Fragment>
+      <HeaderAuthControl />
+      <LogInForm />
+    </React.Fragment>
   </Provider>
 );
