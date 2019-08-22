7486c7b51a5ae3dd37ddbebed3ca01c544c34283
Elizaveta Dokshina <sabitova_liza@icloud.com>
2019-08-22 16:19:49 +0000

    Add console_sock option to cluster.cfg
    Add new cluster.cfg argument
    Add it to argparse cluster_opts
    Call console.listen in cluster.cfg if this param is passed
    Fix conftest Server.init args

---
+++ test/integration/console_sock_test.lua
@@ -0,0 +1,39 @@
+local fio = require('fio')
+local socket = require('socket')
+
+local t = require('luatest')
+local g = t.group('console_sock')
+
+local test_helper = require('test.helper')
+local helpers = require('cluster.test_helpers')
+
+local server
+local tmpdir
+
+g.before_all = function()
+    tmpdir = fio.tempdir()
+    server = helpers.Server:new({
+        alias = 'server',
+        workdir = tmpdir,
+        command = test_helper.server_command,
+        advertise_port = 33001,
+        http_port = 8080,
+        cluster_cookie = 'super-cluster-cookie',
+        env = {
+            TARANTOOL_CONSOLE_SOCK = fio.pathjoin(tmpdir, '/foo.sock')
+        },
+    })
+    server:start()
+    t.helpers.retrying({}, function() server:graphql({query = '{}'}) end)
+end
+
+g.after_all = function()
+    server:stop()
+    fio.rmtree(tmpdir)
+end
+
+g.test_console_sock = function()
+    local s = socket.tcp_connect('unix/', fio.pathjoin(tmpdir, 'foo.sock'))
+    t.assertTrue(s ~= nil)
+    t.assertTrue(s:read('\n'):match('Tarantool.*%(Lua console%)') ~= nil)
+end
