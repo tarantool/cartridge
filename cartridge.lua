421fb8e7771ddf5dcf77b3949cb739796fc311c4
Max Melentiev <m.melentiev@corp.mail.ru>
2019-09-02 19:19:55 +0300

    Emulate NOTIFY_SOCKET on tarantool 2.2.0, 2.2.1 as well

--- cartridge.lua
+++ cartridge.lua
@@ -495,13 +495,15 @@ local function cfg(opts, box_opts)
     local tnt_version = _TARANTOOL:split('.')
     local tnt_major = tonumber(tnt_version[1])
     local tnt_minor = tonumber(tnt_version[2])
-    if tnt_major < 2 or (tnt_major == 2 and tnt_minor < 2) then
-      local notify_socket = os.getenv('NOTIFY_SOCKET')
-      if notify_socket then
-          local socket = require('socket')
-          local sock = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
-          sock:sendto('unix/', notify_socket, 'READY=1')
-      end
+    local tnt_patch = tonumber(tnt_version[3]:split('-')[1])
+    if (tnt_major < 2) or (tnt_major == 2 and tnt_minor < 2) or
+            (tnt_major == 2 and tnt_minor == 2 and tnt_patch < 2) then
+        local notify_socket = os.getenv('NOTIFY_SOCKET')
+        if notify_socket then
+            local socket = require('socket')
+            local sock = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
+            sock:sendto('unix/', notify_socket, 'READY=1')
+        end
     end
 
     return true
