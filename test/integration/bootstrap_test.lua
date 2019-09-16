7bab591a047352a225e741455a2c53fcf2ede021
HustonMmmavr <huston.mavr@gmail.com>
2019-09-16 15:18:50 +0300

    Replace following python tests with lua 1. Replaced test_bootstrap.py with bootstrap_test.lua 2. Replaced test_vshardless.py with vshardless_test.lua

---
+++ test/integration/bootstrap_test.lua
@@ -0,0 +1,63 @@
+local fio = require('fio')
+local t = require('luatest')
+local g = t.group('bootstrap')
+
+local test_helper = require('test.helper')
+local helpers = require('cartridge.test-helpers')
+
+g.before_all = function()
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = test_helper.server_command,
+        replicasets = {
+            {
+                uuid = helpers.uuid('a'),
+                roles = {'myrole'},
+                servers = {
+                    {
+                        alias = 'master',
+                        instance_uuid = helpers.uuid('a', 'a', 1)
+                    },
+                    {
+                        alias = 'replica',
+                        instance_uuid = helpers.uuid('a', 'a', 2)
+                    }
+                },
+            },
+        },
+    })
+    g.cluster:start()
+end
+
+g.after_all = function()
+    g.cluster:stop()
+    fio.rmtree(g.cluster.datadir)
+end
+
+function g.test_restart_one()
+    local master = g.cluster:server('master')
+    master:stop()
+    master:start()
+
+    t.helpers.retrying({timeout = 5}, function()
+        master:graphql({query = '{}'})
+    end)
+end
+
+function g.test_restart_two()
+    local master = g.cluster:server('master')
+    local replica = g.cluster:server('replica')
+
+    master:stop()
+    replica:stop()
+    master:start()
+    replica:start()
+
+    t.helpers.retrying({timeout = 5}, function()
+        master:graphql({query = '{}'})
+    end)
+
+    t.helpers.retrying({timeout = 5}, function()
+        replica:graphql({query = '{}'})
+    end)
+end
---
+++ test/integration/vshardless_test.lua
@@ -0,0 +1,119 @@
+local fio = require('fio')
+local t = require('luatest')
+local g = t.group('vshardless')
+
+local test_helper = require('test.helper')
+local helpers = require('cartridge.test-helpers')
+
+g.before_all = function()
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = fio.pathjoin(test_helper.root, 'test', 'integration', 'srv_vshardless.lua'),
+        use_vshard = false,
+        replicasets = {
+            {
+                alias = 'replica',
+                uuid = helpers.uuid('a'),
+                roles = {},
+                servers = {
+                    {
+                        alias = 'main',
+                        instance_uuid = helpers.uuid('a', 'a', 1)
+                    }
+                },
+            },
+        },
+    })
+    g.cluster:start()
+end
+
+g.after_all = function()
+    g.cluster:stop()
+    fio.rmtree(g.cluster.datadir)
+end
+
+function g.test_edit_replicaset()
+    local replica_uuid = g.cluster.replicasets[1].uuid
+
+    local edit_replicaset = function(roles)
+        g.cluster:server('main'):graphql({
+            query = [[
+                mutation(
+                    $uuid: String!
+                    $roles: [String!]!
+                ) {
+                    edit_replicaset(
+                        uuid: $uuid
+                        roles: $roles
+                    )
+                }
+            ]],
+            variables = {
+                uuid = replica_uuid, roles = roles
+            }
+        })
+    end
+
+    t.assert_error_msg_contains(
+        string.format(
+            'replicasets[%s] can not enable unknown role "vshard-router"',
+            replica_uuid
+        ), edit_replicaset, {'vshard-router'}
+    )
+
+    t.assert_error_msg_contains(
+        string.format(
+            'replicasets[%s] can not enable unknown role "vshard-storage"',
+            replica_uuid
+        ), edit_replicaset, {'vshard-storage'}
+    )
+end
+
+function g.test_package_loaded()
+    g.cluster:server('main').net_box:eval([[
+        assert( package.loaded['cartridge.roles.vshard-router'] == nil )
+        assert( package.loaded['cartridge.roles.vshard-storage'] == nil )
+    ]])
+end
+
+function g.test_config()
+    local server_conn = g.cluster:server('main').net_box
+    local resp = server_conn:eval([[
+        local cartridge = require('cartridge')
+        return cartridge.config_get_readonly('vshard')
+    ]])
+
+    t.assert_equals(resp, {bucket_count = 30000, bootstrapped = false})
+
+    local resp = server_conn:eval([[
+        local cartridge = require('cartridge')
+        return cartridge.config_get_readonly('vshard_groups')
+    ]])
+    t.assert_nil(resp)
+end
+
+function g.test_api()
+    local resp = g.cluster:server('main'):graphql({
+        query = [[
+            {
+                cluster {
+                    can_bootstrap_vshard
+                    vshard_bucket_count
+                    vshard_known_groups
+                    vshard_groups {
+                        name
+                        bucket_count
+                        bootstrapped
+                    }
+                }
+            }
+        ]]
+    })
+
+    t.assert_equals(resp['data']['cluster'], {
+        can_bootstrap_vshard = false,
+        vshard_bucket_count = 0,
+        vshard_known_groups = {},
+        vshard_groups = {},
+    })
+end
