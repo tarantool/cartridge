5ee5a58df77b838db14c45758150f65a1cead621
HustonMmmavr <huston.mavr@gmail.com>
2019-10-28 18:46:15 +0300

    Replace python tests with lua
    1. Replaced test_rpc.py with rpc_test.lua
    2. Replaced test_disable.py with disable_test.lua

---
+++ test/integration/disable_test.lua
@@ -0,0 +1,64 @@
+local fio = require('fio')
+local t = require('luatest')
+local g = t.group('disable')
+
+local test_helper = require('test.helper')
+local helpers = require('cartridge.test-helpers')
+
+g.before_all = function()
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = test_helper.server_command,
+        cookie = 'test-cluster-cookie',
+
+        replicasets = {
+            {
+                uuid = helpers.uuid('a'),
+                roles = {'vshard-router'},
+                servers = {{
+                    alias = 'survivor',
+                    instance_uuid = helpers.uuid('a', 'a', 1),
+                    advertise_port = 13301,
+                    http_port = 8081,
+                }}
+            }, {
+                uuid = helpers.uuid('b'),
+                roles = {'vshard-storage'},
+                servers = {{
+                    alias = 'victim',
+                    instance_uuid = helpers.uuid('b', 'b', 1),
+                    advertise_port = 13302,
+                    http_port = 8082,
+                }}
+            }
+        }
+    })
+    g.cluster:start()
+end
+
+g.after_all = function()
+    g.cluster:stop()
+    fio.rmtree(g.cluster.datadir)
+end
+
+function g.test_api_disable()
+    g.cluster:server('victim'):stop()
+
+    g.cluster.main_server:graphql({query = [[
+        mutation {
+            cluster { disable_servers(uuids: ["bbbbbbbb-bbbb-0000-0000-000000000001"]) }
+        }
+    ]]})
+
+    local res = g.cluster.main_server:graphql({query = [[
+        {
+            servers(uuid: "bbbbbbbb-bbbb-0000-0000-000000000001") {
+                disabled
+            }
+        }
+    ]]})
+
+    local servers = res.data.servers
+    t.assert_equals(#servers, 1)
+    t.assert_equals(servers[1], {disabled = true})
+end
---
+++ test/integration/rpc_test.lua
@@ -0,0 +1,100 @@
+local fio = require('fio')
+local t = require('luatest')
+local g = t.group('rpc')
+
+local test_helper = require('test.helper')
+local helpers = require('cartridge.test-helpers')
+
+g.before_all = function()
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = test_helper.server_command,
+        cookie = 'test-cluster-cookie',
+
+        replicasets = {
+            {
+                uuid = helpers.uuid('a'),
+                roles = {},
+                servers = {{
+                    alias = 'A1',
+                    instance_uuid = helpers.uuid('a', 'a', 1),
+                    advertise_port = 13301,
+                    http_port = 8081,
+                }}
+            }, {
+                uuid = helpers.uuid('b'),
+                roles = {'myrole'},
+                servers = {
+                    {
+                        alias = 'B1',
+                        instance_uuid = helpers.uuid('b', 'b', 1),
+                        advertise_port = 13302,
+                        http_port = 8082,
+                    },{
+                        alias = 'B2',
+                        instance_uuid = helpers.uuid('b', 'b', 2),
+                        advertise_port = 13303,
+                        http_port = 8083,
+                    }
+                }
+            }
+        }
+    })
+    g.cluster:start()
+end
+
+g.after_all = function()
+    g.cluster:stop()
+    fio.rmtree(g.cluster.datadir)
+end
+
+
+local function rpc_call(server, role_name, fn_name, args, kv_args)
+    local res, err = server.net_box:eval([[
+        local rpc = require('cartridge.rpc')
+        return rpc.call(...)
+    ]], {role_name, fn_name, args, kv_args})
+    return res, err
+end
+
+function g.test_api()
+    local server = g.cluster:server('A1')
+
+    local res, err = rpc_call(server, 'myrole', 'get_state')
+    t.assert_nil(err)
+    t.assert_equals(res, 'initialized')
+
+    local res, err = rpc_call(server, 'myrole', 'fn_undefined')
+    t.assert_nil(res)
+    t.assert_equals(err.err, 'Role "myrole" has no method "fn_undefined"')
+
+    local res, err = rpc_call(server, 'unknown-role', 'fn_undefined')
+    t.assert_nil(res)
+    t.assert_equals(err.err, 'No remotes with role "unknown-role" available')
+end
+
+function g.test_errors()
+    local res, err = rpc_call(
+        g.cluster:server('A1'), 'myrole', 'throw', {'Boo'}, {leader_only = true}
+    )
+    t.assert_nil(res)
+    t.assert_equals(err.err, 'Boo')
+    t.assert_equals(err.class_name, 'RemoteCallError')
+    t.assert_str_icontains(err.str, 'during net.box call to localhost:13302')
+
+    local res, err = rpc_call(
+        g.cluster:server('B1'), 'myrole', 'throw', {'Moo'}, {leader_only=true}
+    )
+    t.assert_nil(res)
+    t.assert_equals(err.err, 'Moo')
+    t.assert_equals(err.class_name, 'RemoteCallError')
+    t.assert_not_str_icontains(err.str, 'during net.box call')
+end
+
+function g.test_routing()
+    local res, err = rpc_call(
+        g.cluster:server('B2'), 'myrole', 'is_master', nil, {leader_only=true}
+    )
+    t.assert_nil(err)
+    t.assert_equals(res, true)
+end
