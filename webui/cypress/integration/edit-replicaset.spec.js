d1ea92ec76f3e65f0905605dd020285be4d84389
Alla Khintal <feeramail@gmail.com>
2019-10-29 19:10:31 +0300

    Add default group test (#316)
    Also fix failing cypress test and nail graphql-cli version in CI environment

--- webui/cypress/integration/edit-replicaset.spec.js
+++ webui/cypress/integration/edit-replicaset.spec.js
@@ -10,8 +10,7 @@ describe('Edit Replica Set', () => {
     cy.visit(Cypress.config('baseUrl'));
     cy.get('li').contains('router1-do-not-use-me').closest('li').find('button').contains('Edit').click();
     cy.get('.meta-test__EditReplicasetModal input[name="alias"]')
-      .type('{selectall}{del}')
-      .type('editedRouter')
+      .type('{selectall}editedRouter')
       .should('have.value', 'editedRouter');
     cy.get('.meta-test__EditReplicasetModal input[name="roles"][value="myrole"]').uncheck({ force: true });
 
