7c8d005b08c63c9e4b9eddd9db56de4e4f4faf61
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-11-25 12:38:45 +0300

    Automate scm-1 rockspec uploading and releases (#352)

--- release.sh
+++ release.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-TAG=$(git describe --exact-match HEAD 2>/dev/null)
-if [ -z "$TAG" ]
+set -e
+
+if [ -n "$CI_COMMIT_TAG" ]
 then
-	echo "Skipping release: no git tag found."
-	exit 0
+    TAG="$CI_COMMIT_TAG"
+else
+    TAG=$(git describe --exact-match HEAD 2>/dev/null)
 fi
 
-echo TAG = \"$TAG\"
+if [ -z "$TAG" ]
+then
+    echo "Skipping release: no git tag found."
+    exit 0
+fi
+
+echo "Preparing release \"$TAG\""
 mkdir -p release
 sed -e "s/branch = '.\+'/tag = '$TAG'/g" \
     -e "s/version = '.\+'/version = '$TAG-1'/g" \
     -e "s/BUILD_DOC = '.\+'/BUILD_DOC = 'YES'/g" \
-    cluster-scm-1.rockspec > release/cluster-$TAG-1.rockspec
+    cartridge-scm-1.rockspec > release/cartridge-$TAG-1.rockspec
 
-tarantoolctl rocks make release/cluster-$TAG-1.rockspec
-tarantoolctl rocks pack cluster $TAG && mv cluster-$TAG-1.all.rock release/
+tarantoolctl rocks make release/cartridge-$TAG-1.rockspec
+tarantoolctl rocks pack cartridge $TAG && mv cartridge-$TAG-1.all.rock release/
 
 mkdir -p release-doc
-cp -RT doc/ release-doc/cluster-$TAG-1
+cp -RT doc/ release-doc/cartridge-$TAG-1
