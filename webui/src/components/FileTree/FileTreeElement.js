d14ac1c30bdf71fe28a201fd6d0bf672f5856ebc
Roman Babaev <r.babaev@corp.mail.ru>
2019-11-29 14:58:34 +0300

    Add files tree and file operations to code editor

---
+++ webui/src/components/FileTree/FileTreeElement.js
@@ -0,0 +1,175 @@
+// @flow
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { css, cx } from 'emotion';
+import {
+  Button,
+  ControlsPanel,
+  DotIndicator,
+  IconChevron,
+  IconCreateFolder,
+  IconCreateFile,
+  IconDelete,
+  IconEdit,
+  IconFile,
+  IconFolder,
+  Text
+} from '@tarantool.io/ui-kit';
+import type { TreeFileItem } from 'src/store/selectors/filesSelectors';
+import type { FileItem } from 'src/store/reducers/files.reducer';
+
+const styles = {
+  element: css`
+    position: relative;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    height: 34px;
+    padding-right: 8px;
+    user-select: none;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #ffffff;
+    }
+
+    .FileTreeElement__btns {
+      display: none;
+    }
+
+    &:hover > .FileTreeElement__btns {
+      display: flex;
+    }
+  `,
+  active: css`
+    background-color: #ffffff;
+  `,
+  fileName: css`
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `,
+  iconChevron: css`
+    margin: 4px;
+    fill: rgba(0, 0, 0, 0.65);
+  `,
+  iconChevronFile: css`
+    visibility: hidden;
+  `,
+  fileIcon: css`
+    margin: 4px;
+  `,
+  buttonsPanel: css`
+    position: absolute;
+    top: calc(50% - 9px);
+    right: 8px;
+    display: flex;
+    flex-wrap: nowrap;
+    background-color: #ffffff;
+  `,
+  fileActionBtn: css`
+    line-height: 16px;
+    padding: 0 2px;
+  `,
+  fileActionBtnMargin: css`
+    margin-right: 8px;
+  `
+};
+
+type FileTreeElementProps = {
+  active?: boolean,
+  children?: React.Node,
+  className?: string,
+  expanded?: boolean,
+  file: FileItem,
+  level?: number,
+  onDelete: (id: string) => void,
+  onExpand: (id: string) => void,
+  onFileCreate: (parentId: string) => void,
+  onFileOpen: (id: string) => void,
+  onFolderCreate: (parentId: string) => void,
+  onRename: (id: string) => void
+}
+
+export const FileTreeElement = (
+  {
+    active,
+    className,
+    children,
+    expanded,
+    file,
+    level,
+    onDelete,
+    onExpand,
+    onFileCreate,
+    onFileOpen,
+    onFolderCreate,
+    onRename
+  }: FileTreeElementProps
+) => {
+  const Icon = file.type === 'folder' ? IconFolder : IconFile;
+
+  return (
+    <React.Fragment>
+      <li
+        className={cx(
+          styles.element,
+          { [styles.active]: active },
+          className
+        )}
+        onClick={e => {
+          file.type === 'folder'
+            ? onExpand(file.fileId)
+            : onFileOpen(file.fileId)
+        }}
+        style={{
+          paddingLeft: (level || 0) * 20
+        }}
+        title={file.fileName}
+      >
+        <IconChevron
+          className={cx(styles.iconChevron, { [styles.iconChevronFile]: file.type !== 'folder' })}
+          direction={expanded ? 'down' : 'right' }
+        />
+        <Icon className={styles.fileIcon} opened={expanded} />
+        <Text className={styles.fileName}>{file.fileName}</Text>
+        {!file.saved && <DotIndicator state='bad' />}
+        <div className={cx(styles.buttonsPanel, 'FileTreeElement__btns')}>
+          {file.type === 'folder' && (
+            <React.Fragment>
+              <Button
+                className={styles.fileActionBtn}
+                intent='plain'
+                size='xs'
+                icon={IconCreateFolder}
+                onClick={e => { e.stopPropagation(); onFolderCreate(file.fileId); }}
+              />
+              <Button
+                className={cx(styles.fileActionBtn, styles.fileActionBtnMargin)}
+                intent='plain'
+                size='xs'
+                icon={IconCreateFile}
+                onClick={e => { e.stopPropagation(); onFileCreate(file.fileId); }}
+              />
+            </React.Fragment>
+          )}
+          <Button
+            className={styles.fileActionBtn}
+            intent='plain'
+            size='xs'
+            icon={IconEdit}
+            onClick={e => { e.stopPropagation(); onRename(file.fileId); }}
+          />
+          <Button
+            className={styles.fileActionBtn}
+            intent='plain'
+            size='xs'
+            icon={IconDelete}
+            onClick={e => { e.stopPropagation(); onDelete(file.fileId); }}
+          />
+        </div>
+      </li>
+      {expanded && children}
+    </React.Fragment>
+  );
+};
---
+++ webui/src/components/FileTree/index.js
@@ -0,0 +1,101 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import { css, cx } from 'emotion';
+import { Button, ControlsPanel, Text, IconRefresh } from '@tarantool.io/ui-kit';
+import type { TreeFileItem } from 'src/store/selectors/filesSelectors';
+import type { FileItem } from 'src/store/reducers/files.reducer';
+import { FileTreeElement } from './FileTreeElement';
+
+const renderTree = (treeNode: Object, prop: string, render: Function, level: number) => {
+  const children = (treeNode[prop] || []).map(x => renderTree(x, prop, render, level + 1))
+  return render(treeNode, children, level)
+}
+
+const styles = {
+  tree: css`
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  `
+};
+
+type FileTreeProps = {
+  className?: string,
+  tree: Array<TreeFileItem>,
+  selectedFile: FileItem | null,
+  onFileCreate: (parentId: string) => void,
+  onDelete: (id: string) => void,
+  onFileOpen: (id: string) => void,
+  onFolderCreate: (parentId: string) => void,
+  onRename: (id: string) => void,
+};
+
+type FileTreeState = {
+  expandedEntries: string[]
+};
+
+export class FileTree extends React.Component<FileTreeProps, FileTreeState> {
+  state = {
+    expandedEntries: []
+  };
+
+  // TODO: по componentWillUpdate убрать из списка файлы, которые были удалены
+  expandEntry = (id: string) => {
+    const { expandedEntries } = this.state;
+
+    if (expandedEntries.includes(id)) {
+      this.setState({
+        expandedEntries: expandedEntries.filter(i => i !== id )
+      });
+    } else {
+      this.setState({
+        expandedEntries: [...expandedEntries, id]
+      });
+    }
+  }
+
+  render() {
+    const {
+      className,
+      tree = [],
+      selectedFile,
+      onDelete,
+      onFileCreate,
+      onFileOpen,
+      onFolderCreate,
+      onRename
+    } = this.props;
+
+    const { expandedEntries } = this.state;
+
+    return (
+      <ul className={cx(styles.tree, className)}>
+        {tree.map(
+          x => renderTree(
+            x,
+            'items',
+            (item, children, level) => (
+              <FileTreeElement
+                key={item.fileId}
+                file={item}
+                active={selectedFile && (selectedFile.fileId === item.fileId)}
+                level={level}
+                expanded={expandedEntries.includes(item.fileId)}
+                onDelete={onDelete}
+                onExpand={this.expandEntry}
+                onFileCreate={onFileCreate}
+                onFileOpen={onFileOpen}
+                onFolderCreate={onFolderCreate}
+                onRename={onRename}
+              >
+                {children}
+              </FileTreeElement>
+            ),
+            0
+          )
+        )}
+      </ul>
+    );
+  }
+}
