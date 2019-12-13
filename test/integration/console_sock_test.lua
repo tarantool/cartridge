114e00760c903e1b1016e184663b4ae69582fb62
Pavel Shipilov <splkmaxdk@gmail.com>
2019-12-14 02:11:50 +0300

    Fix console socket test (add: wait notify socket)

--- test/integration/console_sock_test.lua
+++ test/integration/console_sock_test.lua
@@ -1,5 +1,6 @@
 local fio = require('fio')
 local socket = require('socket')
+local log = require('log')
 
 local t = require('luatest')
 local g = t.group('console_sock')
@@ -10,7 +11,24 @@ local helpers = require('cartridge.test-helpers')
 local server
 local tmpdir
 
+local function wait_notify()
+    local notify_socket_name = server.env.TARANTOOL_NOTIFY_SOCK
+    local notify_socket = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
+    assert(notify_socket:bind('unix/', notify_socket_name), notify_socket:error())
+    fio.chmod(notify_socket_name, tonumber('0666', 8))
+    while true do
+        if notify_socket:readable(1) then
+            local msg = notify_socket:recv()
+            if msg:match('READY=1') then
+                fio.unlink(notify_socket_name)
+                return
+            end
+        end
+    end
+end
+
 g.before_all = function()
+    log.level(7)
     tmpdir = fio.tempdir()
     server = helpers.Server:new({
         alias = 'server',
@@ -20,11 +38,13 @@ g.before_all = function()
         http_port = 8080,
         cluster_cookie = 'super-cluster-cookie',
         env = {
-            TARANTOOL_CONSOLE_SOCK = fio.pathjoin(tmpdir, '/foo.sock')
+            TARANTOOL_CONSOLE_SOCK = fio.pathjoin(tmpdir, '/foo.sock'),
+            TARANTOOL_NOTIFY_SOCK = fio.pathjoin(tmpdir, '/notify.sock')
         },
     })
     server:start()
-    t.helpers.retrying({}, function() server:graphql({query = '{}'}) end)
+    wait_notify()
+    --t.helpers.retrying({}, function() server:graphql({query = '{}'}) end)
 end
 
 g.after_all = function()
@@ -35,7 +55,7 @@ end
 g.test_console_sock = function()
     local s = socket.tcp_connect('unix/', fio.pathjoin(tmpdir, 'foo.sock'))
     t.assertTrue(s ~= nil)
-    local greeting = s:read('\n')
+        local greeting = s:read('\n')
     t.assertNotNil(greeting)
     t.assertStrMatches(greeting:strip(), 'Tarantool.*%(Lua console%)')
 end
