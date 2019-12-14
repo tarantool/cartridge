c5a81974218eea88518c23f015a9ee5ce49dcdcc
Pavel Shipilov <splkmaxdk@gmail.com>
2019-12-14 11:25:28 +0300

    Fix bug wait socket (add: build socket)

--- test/integration/console_sock_test.lua
+++ test/integration/console_sock_test.lua
@@ -11,16 +11,20 @@ local helpers = require('cartridge.test-helpers')
 local server
 local tmpdir
 
+local notify_socket 
+
+local function build_notify_socket()
+    notify_socket = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
+    assert(notify_socket:bind('unix/', server.env.TARANTOOL_NOTIFY_SOCK), notify_socket:error())
+    fio.chmod(server.env.TARANTOOL_NOTIFY_SOCK, tonumber('0666', 8))
+end
+
 local function wait_notify()
-    local notify_socket_name = server.env.TARANTOOL_NOTIFY_SOCK
-    local notify_socket = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
-    assert(notify_socket:bind('unix/', notify_socket_name), notify_socket:error())
-    fio.chmod(notify_socket_name, tonumber('0666', 8))
     while true do
         if notify_socket:readable(1) then
             local msg = notify_socket:recv()
             if msg:match('READY=1') then
-                fio.unlink(notify_socket_name)
+                fio.unlink(server.env.TARANTOOL_NOTIFY_SOCK)
                 return
             end
         end
@@ -42,6 +46,7 @@ g.before_all = function()
             TARANTOOL_NOTIFY_SOCK = fio.pathjoin(tmpdir, '/notify.sock')
         },
     })
+    build_notify_socket()
     server:start()
     wait_notify()
     --t.helpers.retrying({}, function() server:graphql({query = '{}'}) end)
@@ -55,7 +60,7 @@ end
 g.test_console_sock = function()
     local s = socket.tcp_connect('unix/', fio.pathjoin(tmpdir, 'foo.sock'))
     t.assertTrue(s ~= nil)
-        local greeting = s:read('\n')
+    local greeting = s:read('\n')
     t.assertNotNil(greeting)
     t.assertStrMatches(greeting:strip(), 'Tarantool.*%(Lua console%)')
 end
