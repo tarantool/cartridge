d6ec32f53c79d8189174f9b1a96cd8fbc3ec6a8d
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-12-16 00:24:44 +0300

    Rename cypress test accordingly

--- test/cypress/woauth_test.lua
+++ test/cypress/woauth_test.lua
@@ -1,6 +1,6 @@
 local fio = require('fio')
 local t = require('luatest')
-local g = t.group('withoutUsersPage')
+local g = t.group('cypress_woauth')
 
 local test_helper = require('test.helper')
 local helpers = require('cartridge.test-helpers')
