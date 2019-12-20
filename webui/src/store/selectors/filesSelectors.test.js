12ae1981e77534ef2a74d5167f4450eb77e8c372
iv-ov <iv-ov@users.noreply.github.com>
2019-12-20 16:46:26 +0300

    Fixed unit tests for selector `selectFileTree()`

--- webui/src/store/selectors/filesSelectors.test.js
+++ webui/src/store/selectors/filesSelectors.test.js
@@ -8,122 +8,151 @@ describe('selectFileTree', () => {
       'path': 'rootFile.ext',
       'parentPath': '',
       'type': 'file',
-      'fileId': 'rootFile.ext',
+      'fileId': '1',
       'fileName': 'rootFile.ext'
     }, {
       'path': 'rootFile2.ext',
       'parentPath': '',
       'type': 'file',
-      'fileId': 'rootFile2.ext',
+      'fileId': '2',
       'fileName': 'rootFile2.ext'
     }, {
       'path': 'rootFolder',
       'parentPath': '',
       'type': 'folder',
-      'fileId': 'rootFolder',
+      'fileId': '3',
       'fileName': 'rootFolder'
     }, {
       'path': 'rootFolder/file.ext',
       'parentPath': 'rootFolder',
       'type': 'file',
-      'fileId': 'rootFolder/file.ext',
-      'fileName': 'rootFolder/file.ext'
+      'fileId': '4',
+      'fileName': 'file.ext'
     }, {
       'path': 'rootFolder2',
       'parentPath': '',
       'type': 'folder',
-      'fileId': 'rootFolder2',
+      'fileId': '5',
       'fileName': 'rootFolder2'
     }, {
       'path': 'rootFolder2/file1.ext',
       'parentPath': 'rootFolder2',
       'type': 'file',
-      'fileId': 'rootFolder2/file1.ext',
-      'fileName': 'rootFolder2/file1.ext'
+      'fileId': '6',
+      'fileName': 'file1.ext'
     }, {
       'path': 'rootFolder2/file2.ext',
       'parentPath': 'rootFolder2',
       'type': 'file',
-      'fileId': 'rootFolder2/file2.ext',
-      'fileName': 'rootFolder2/file2.ext'
+      'fileId': '7',
+      'fileName': 'file2.ext'
     }, {
       'path': 'rootFolder2/subFolder',
       'parentPath': 'rootFolder2',
       'type': 'folder',
-      'fileId': 'rootFolder2/subFolder',
-      'fileName': 'rootFolder2/subFolder'
+      'fileId': '8',
+      'fileName': 'subFolder'
     }, {
       'path': 'rootFolder2/subFolder/file.ext',
-      'parentPath': 'rootFolder2',
+      'parentPath': 'rootFolder2/subFolder',
       'type': 'file',
-      'fileId': 'rootFolder2/subFolder/file.ext',
-      'fileName': 'rootFolder2/subFolder/file.ext'
+      'fileId': '9',
+      'fileName': 'file.ext'
     }, {
       'path': 'rootFolder2/subFolder2',
       'parentPath': 'rootFolder2',
       'type': 'folder',
-      'fileId': 'rootFolder2/subFolder2',
-      'fileName': 'rootFolder2/subFolder2'
+      'fileId': '10',
+      'fileName': 'subFolder2'
     }, {
       'path': 'rootFolder2/subFolder2/file.ext',
       'parentPath': 'rootFolder2/subFolder2',
       'type': 'file',
-      'fileId': 'rootFolder2/subFolder2/file.ext',
-      'fileName': 'rootFolder2/subFolder2/file.ext'
+      'fileId': '11',
+      'fileName': 'file.ext'
     }];
 
     expect(selectFileTree(state)).toMatchObject([
       {
-        fileId: 'rootFolder', path: 'rootFolder',
-        fileName: 'rootFolder', type: 'folder',
+        fileId: '3',
+        path: 'rootFolder',
+        fileName: 'rootFolder',
+        type: 'folder',
         items: [
           {
-            fileId: 'rootFolder/file.ext', path: 'rootFolder/file.ext',
+            fileId: '4', path: 'rootFolder/file.ext',
             fileName: 'file.ext', type: 'file',
           },
         ]
       },
       {
-        fileId: 'rootFolder2', path: 'rootFolder2',
-        fileName: 'rootFolder2', type: 'folder',
+        fileId: '5',
+        path: 'rootFolder2',
+        fileName: 'rootFolder2',
+        type: 'folder',
         items: [
           {
-            fileId: 'rootFolder2/subFolder', path: 'rootFolder2/subFolder',
-            fileName: 'subFolder', type: 'folder',
+            fileId: '8',
+            path: 'rootFolder2/subFolder',
+            parentPath: 'rootFolder2',
+            fileName: 'subFolder',
+            type: 'folder',
             items: [
               {
-                fileId: 'rootFolder2/subFolder/file.ext', path: 'rootFolder2/subFolder/file.ext',
-                fileName: 'file.ext', type: 'file',
+                fileId: '9',
+                path: 'rootFolder2/subFolder/file.ext',
+                parentPath: 'rootFolder2/subFolder',
+                fileName: 'file.ext',
+                type: 'file',
+                items: [],
               },
             ]
           },
           {
-            fileId: 'rootFolder2/subFolder2', path: 'rootFolder2/subFolder2',
+            fileId: '10',
+            path: 'rootFolder2/subFolder2',
+            parentPath: 'rootFolder2',
             fileName: 'subFolder2', type: 'folder',
             items: [
               {
-                fileId: 'rootFolder2/subFolder2/file.ext', path: 'rootFolder2/subFolder2/file.ext',
-                fileName: 'file.ext', type: 'file',
+                fileId: '11',
+                path: 'rootFolder2/subFolder2/file.ext',
+                parentPath: 'rootFolder2/subFolder2',
+                fileName: 'file.ext',
+                type: 'file',
+                items: [],
               },
             ]
           },
           {
-            fileId: 'rootFolder2/file1.ext', path: 'rootFolder2/file1.ext',
-            fileName: 'file1.ext', type: 'file',
+            fileId: '6',
+            path: 'rootFolder2/file1.ext',
+            parentPath: 'rootFolder2',
+            fileName: 'file1.ext',
+            type: 'file',
+            items: [],
           },
           {
-            fileId: 'rootFolder2/file2.ext', path: 'rootFolder2/file2.ext',
-            fileName: 'file2.ext', type: 'file',
-          },
+            fileId: '7',
+            path: 'rootFolder2/file2.ext',
+            parentPath: 'rootFolder2',
+            fileName: 'file2.ext',
+            type: 'file',
+            items: [],
+          }
         ]
       },
       {
-        fileId: 'rootFile.ext', path: 'rootFile.ext',
-        fileName: 'rootFile.ext', type: 'file',
+        fileId: '1',
+        path: 'rootFile.ext',
+        fileName: 'rootFile.ext',
+        type: 'file',
       },
       {
-        fileId: 'rootFile2.ext', path: 'rootFile2.ext',
-        fileName: 'rootFile2.ext', type: 'file',
+        fileId: '2',
+        path: 'rootFile2.ext',
+        fileName: 'rootFile2.ext',
+        type: 'file',
       },
     ]);
   });
@@ -131,7 +160,7 @@ describe('selectFileTree', () => {
 
   it('duplicate files are not repeated', () => {
     const stateWithDuplicateFiles = [
-      { type: 'folder', path: 'duplicate', parentPath: '', deleted: false},
+      { type: 'folder', path: 'duplicate', parentPath: '', deleted: false },
       { type: 'file', path: 'duplicate/files.ext', deleted: false, parentPath: 'duplicate' },
       { type: 'file', path: 'duplicate/files.ext', deleted: false, parentPath: 'duplicate' },
     ];
@@ -184,7 +213,7 @@ describe('selectFileTree', () => {
         path: 'folder',
         type: 'folder',
         items: [
-          {...state[1], items: []},
+          { ...state[1], items: [] },
           {
             parentPath: 'folder',
             path: 'folder/file2.ext',
