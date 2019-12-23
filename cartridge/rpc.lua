2a04628ac2ddf71e4537e91616e0f5affb8882c2
Pavel Shipilov <splkmaxdk@gmail.com>
2019-12-23 19:11:42 +0300

    Fix discription

--- cartridge/rpc.lua
+++ cartridge/rpc.lua
@@ -175,12 +175,13 @@ end
 --   Perform a call only on the replica set leaders.
 --   (default: **false**)
 -- @tparam ?string opts.uri
---   Perform a call only on the replica set uri
+--   Perform a call on this particular uri. Other options are ignored 
+--   (leader_only, prefer_local) as well as member status.
 --   (added in v1.2.0-62)
 -- @param opts.remote_only (*deprecated*) Use `prefer_local` instead.
 -- @param opts.timeout passed to `net.box` `conn:call` options.
 -- @param opts.buffer passed to `net.box` `conn:call` options.
--- @param opts.uri passed to `net.box` `conn:call` options.
+-- @param opts.uri 
 --
 -- @return[1] `conn:call()` result
 -- @treturn[2] nil
