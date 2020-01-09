aff1e018b0f9da48d4641107e060e125701be12b
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2020-01-10 00:54:20 +0300

    Enhance schema editor test coverage

--- webui/cypress/integration/schema-editor.spec.js
+++ webui/cypress/integration/schema-editor.spec.js
@@ -6,20 +6,47 @@ describe('Schema section', () => {
     cy.visit(Cypress.config('baseUrl')+"/admin/cluster/schema");
     cy.get('.monaco-editor textarea').should('have.value', defaultText);
 
+    ////////////////////////////////////////////////////////////////////
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').should('have.value', '');
-    cy.get('.monaco-editor textarea').type('spaces: incorrect');
-    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect');
+    cy.get('.monaco-editor textarea').type('spaces: incorrect-1');
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect-1');
 
     cy.get('button[type="button"]').contains('Validate').click();
-    cy.get('#root').contains(
-      'Bad argument #1 to ddl.check_schema' +
-      ' invalid schema.spaces (?table expected, got string)');
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema');
 
-    cy.reload();
+    cy.get('button[type="button"]').contains('Reload').click();
     cy.get('.monaco-editor textarea').should('have.value', defaultText);
+
+    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
+    cy.get('.monaco-editor textarea').type('spaces: [] # Essentially the same');
+
+    cy.get('button[type="button"]').contains('Validate').click();
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema').should('not.exist');
+    cy.get('#root').contains('Schema is valid');
 
     cy.get('button[type="button"]').contains('Apply').click();
     cy.get('#root').contains('Schema successfully applied');
+
+    ////////////////////////////////////////////////////////////////////
+    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
+    cy.get('.monaco-editor textarea').type('spaces: incorrect-2');
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect-2');
+
+    cy.get('button[type="button"]').contains('Apply').click();
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema');
+
+    cy.get('button[type="button"]').contains('Reload').click();
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: [] # Essentially the same');
+
+    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
+    cy.get('.monaco-editor textarea').type(defaultText);
+    cy.get('.monaco-editor textarea').should('have.value', defaultText);
+
+    cy.get('button[type="button"]').contains('Apply').click();
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema').should('not.exist');
+    cy.get('#root').contains('Schema successfully applied');
+
+    cy.get('button[type="button"]').contains('Reload').click();
+    cy.get('.monaco-editor textarea').should('have.value', defaultText);
   });
 });
