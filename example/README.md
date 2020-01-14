4ea253db63acbec5f8c237f12892ac793de4e7e6
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2020-01-14 22:28:37 +0300

    Add try-cartridge config draft
    See also #454

---
+++ example/README.md
@@ -0,0 +1,24 @@
+## Try Cartridge
+
+According to @artur-barsegyan's proposal we'll implement new cartridge
+role - `extensions`.
+
+All `extensions/*.lua` files will be loaded as Lua modules.
+They'll be accessible through
+```lua
+local extensions = require('cartridge').service_get('extensions')
+local banking = extensions.get('banking') -- TODO ban cycle deps
+```
+
+Also `extensions/config.yml` describes how to export those modules in
+serverless style. For now we'll support the only event `binary`:
+```yml
+functions:
+  foo:
+    module: banking
+    handler: transfer_money
+    events:
+    - binary:
+        _G: __transfer_money
+  # It'll assign _G.__transfer_money = banking.transfer_money
+```
---
+++ example/extensions/banking.lua
@@ -0,0 +1,30 @@
+local checks = require('checks')
+local extensions = require('cartridge').service_get('extensions')
+
+local function customer_add(customer_id, fullname)
+    checks('number', 'string')
+    return box.space.customer:insert(
+        {customer_id, fullname}
+    )
+end
+
+local function account_add(customer_id, account_id, name)
+    checks('number', 'number', 'string')
+    return box.space.account:insert(
+        {customer_id, account_id, name, '0.00'}
+    )
+end
+
+local function transfer_money(account_from, account_to, amount)
+    box.begin()
+        box.space.account:update({account_to}, {{'+', 4, amount}})
+        box.space.account:update({account_from}, {{'-', 4, amount}})
+    box.commit()
+    return true
+end
+
+return {
+    customer_add = customer_add,
+    account_add = account_add,
+    transfer_money = transfer_money,
+}
