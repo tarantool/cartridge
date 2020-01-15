2b7ab6a3abddbb12b9b94892be138f6b93b004f5
Alla <feeramail@gmail.com>
2020-01-15 15:53:51 +0400

    Replace schema-editor test

--- webui/cypress/integration/schema-editor.spec.js
+++ webui/cypress/integration/schema-editor.spec.js
@@ -1,64 +1,52 @@
 describe('Schema section', () => {
-  it('Schema with bootstrap', () => {
+  it('Empty', () => {
     const selectAllKeys = Cypress.platform == 'darwin' ? '{cmd}a' : '{ctrl}a';
     const defaultText = '---\nspaces: []\n...\n';
 
     cy.visit(Cypress.config('baseUrl')+"/admin/cluster/schema");
     cy.get('.monaco-editor textarea').should('have.value', defaultText);
 
-    //Apply and Reload buttons
+    ////////////////////////////////////////////////////////////////////
+    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
+    cy.get('.monaco-editor textarea').type('spaces: incorrect-1');
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect-1');
+
+    cy.get('button[type="button"]').contains('Validate').click();
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema');
+
+    cy.get('button[type="button"]').contains('Reload').click();
+    cy.get('.monaco-editor textarea').should('have.value', defaultText);
+
+    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
+    cy.get('.monaco-editor textarea').type('spaces: [] # Essentially the same');
+
+    cy.get('button[type="button"]').contains('Validate').click();
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema').should('not.exist');
+    cy.get('#root').contains('Schema is valid');
+
     cy.get('button[type="button"]').contains('Apply').click();
     cy.get('#root').contains('Schema successfully applied');
 
+    ////////////////////////////////////////////////////////////////////
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').type('---\nspaces: []\n...\ntest error parsing');
+    cy.get('.monaco-editor textarea').type('spaces: incorrect-2');
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect-2');
+
     cy.get('button[type="button"]').contains('Apply').click();
-    cy.get('#root').contains(
-      'Error parsing section "schema.yml":' + 
-      ' did not find expected <document start> at document');
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema');
+
+    cy.get('button[type="button"]').contains('Reload').click();
+    cy.get('.monaco-editor textarea').should('have.value', 'spaces: [] # Essentially the same');
 
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').should('have.value', '');
-    cy.get('.monaco-editor textarea').type('spaces: incorrect');
-    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect');
-    cy.get('button[type="button"]').contains('Apply').click();
-    cy.get('#root').contains(
-      'Bad argument #1 to ddl.check_schema' +
-      ' invalid schema.spaces (?table expected, got string)');
+    cy.get('.monaco-editor textarea').type(defaultText);
+    cy.get('.monaco-editor textarea').should('have.value', defaultText);
 
-    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').should('have.value', '');
     cy.get('button[type="button"]').contains('Apply').click();
-    cy.get('#root').contains('Unknown error at localhost');
+    cy.get('#root').contains('Bad argument #1 to ddl.check_schema').should('not.exist');
+    cy.get('#root').contains('Schema successfully applied');
 
     cy.get('button[type="button"]').contains('Reload').click();
     cy.get('.monaco-editor textarea').should('have.value', defaultText);
-
-    //Validate button
-    cy.get('button[type="button"]').contains('Validate').click();
-    cy.get('#root').contains('Schema is valid');
-    
-    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').type('---\nspaces: []\n...\ntest error parsing');
-    cy.get('button[type="button"]').contains('Validate').click();
-    cy.get('#root').contains('did not find expected <document start> at document');
-
-    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').should('have.value', '');
-    cy.get('.monaco-editor textarea').type('spaces: incorrect');
-    cy.get('.monaco-editor textarea').should('have.value', 'spaces: incorrect');
-    cy.get('button[type="button"]').contains('Validate').click();
-    cy.get('#root').contains(
-      'Bad argument #1 to ddl.check_schema' +
-      ' invalid schema.spaces (?table expected, got string)');
-
-    cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    cy.get('.monaco-editor textarea').should('have.value', '');
-    cy.get('button[type="button"]').contains('Validate').click();
-    cy.get('#root').contains('attempt to index local \'err\' (a nil value)');
-    
-    cy.get('button[type="button"]').contains('Reload').click();
-    cy.get('.monaco-editor textarea').should('have.value', defaultText);
-
   });
 });
