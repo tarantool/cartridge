95de8166e811157e5a6d4b7f5f239b3bb4fcdfdb
Ivan Ovchinnikov <iv-ov@users.noreply.github.com>
2020-01-30 18:49:37 +0300

    Fix file creation bug in WebUI (#511)
    A new file wasn't committed to the configuration unless it was opened
    after creation
    
    Close #496 
    
    Co-authored-by: Alla Khintal <feeramail@gmail.com>

---
+++ webui/cypress/integration/code-empty-page.spec.js
@@ -0,0 +1,13 @@
+describe('Code page', () => {
+
+    it('Empty code page', () => {
+      const selectAllKeys = Cypress.platform == 'darwin' ? '{cmd}a' : '{ctrl}a';
+      //const defaultText = 'Please select a file';
+  
+      cy.visit(Cypress.config('baseUrl')+"/admin/cluster/code");
+      cy.get('#root').contains('Please select a file');
+      cy.get('button[type="button"]').contains('Apply').click();
+      cy.get('#root').contains('Files successfuly applied');
+    });
+  });
+  
