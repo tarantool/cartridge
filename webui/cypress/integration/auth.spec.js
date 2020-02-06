98eb0e30f7d1823f8fc0c9182fdf17cf1f5c0fb1
iv-ov <iv-ov@users.noreply.github.com>
2020-02-06 13:24:02 +0300

    Added check for auth test: welcome message

--- webui/cypress/integration/auth.spec.js
+++ webui/cypress/integration/auth.spec.js
@@ -9,6 +9,13 @@ describe('Auth', () => {
   it('Login and Enable Auth', () => {
     cy.visit(Cypress.config('baseUrl'));
     cy.get('.meta-test__LoginBtn').click();
+
+    // check that welcome_message & checkbox do not exist
+    // and login button is enabled
+    cy.get('.meta-test__LoginForm').contains('Happy families are').should('not.exist');
+    cy.get('.meta-test__LoginForm input[name="isAgreeChecked"]').should('not.exist');
+    cy.get('.meta-test__LoginFormBtn').should('be.enabled');
+
     cy.get('.meta-test__LoginForm input[name="username"]')
       .type('admin')
       .should('have.value', 'admin');
