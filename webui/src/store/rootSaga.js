c49d254d8116623ecd06c2c007ad05536568e859
Roman Babaev <kotfly@gmail.com>
2020-02-07 20:18:07 +0300

    Remember last open file in code-editor WebUI (#546)
    * Automatically open a file at first run
    * Keep file tree expanded by default.

--- webui/src/store/rootSaga.js
+++ webui/src/store/rootSaga.js
@@ -4,6 +4,7 @@ import { saga as appSaga } from 'src/store/saga/app.saga';
 import { saga as authSaga } from 'src/store/saga/auth.saga';
 import { saga as clusterPageSaga } from 'src/store/saga/clusterPage.saga';
 import { saga as clusterInstancePageSaga } from 'src/store/saga/clusterInstancePage.saga';
+import { saga as editorSaga } from 'src/store/saga/editor.saga';
 import { saga as schemaSaga } from 'src/store/saga/schema.saga';
 import { saga as filesSaga } from 'src/store/saga/files.saga';
 import { saga as usersSaga } from 'src/store/saga/users.saga';
@@ -14,6 +15,7 @@ export default function* rootSaga() {
     authSaga,
     clusterPageSaga,
     clusterInstancePageSaga,
+    editorSaga,
     schemaSaga,
     filesSaga,
     usersSaga
