76c02960338da43b5306d5b3273c155b2dd2465c
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2020-02-27 17:08:05 +0300

    Fix flaky CI tests by engaging tmpfs
    As investigation [shows](https://github.com/tarantool/cartridge/issues/612),
    the root of timeouts is in slow IO on out Gitlab CI runner.
    
    This patch engages tmpfs (`/dev/shm`) for saving temporary test files.
    
    Close #612

--- test/helper.lua
+++ test/helper.lua
@@ -1,7 +1,22 @@
 local fio = require('fio')
+local digest = require('digest')
 local helpers = table.copy(require('cartridge.test-helpers'))
 
 helpers.project_root = fio.dirname(debug.sourcedir())
+
+local __fio_tempdir = fio.tempdir
+fio.tempdir = function()
+    local base = os.getenv('TMPDIR')
+    if base == nil or base == '/tmp' then
+        return __fio_tempdir()
+    else
+        local random = digest.urandom(9)
+        local suffix = digest.base64_encode(random, {urlsafe = true})
+        local path = fio.pathjoin(base, 'tmp.cartridge.' .. suffix)
+        fio.mktree(path)
+        return path
+    end
+end
 
 function helpers.entrypoint(name)
     local path = fio.pathjoin(
