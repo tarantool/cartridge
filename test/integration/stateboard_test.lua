cf1b8ffe93975e33979a34e242d733b50f077b03
HustonMmmavr <huston.mavr@gmail.com>
2020-04-07 20:40:44 +0300

    Minimize diff

--- test/integration/stateboard_test.lua
+++ test/integration/stateboard_test.lua
@@ -45,82 +45,85 @@ local function create_client(srv)
 end
 
 function g.test_locks()
-    local session1 = create_client(g.stateboard):get_session()
-    local session2 = create_client(g.stateboard):get_session()
+    local c1 = create_client(g.stateboard):get_session()
+    local c2 = create_client(g.stateboard):get_session()
     local kid = uuid.str()
 
     t.assert_equals(
-        session1:acquire_lock({kid, 'localhost:9'}),
+        c1:acquire_lock({kid, 'localhost:9'}),
         true
     )
     t.assert_equals(
-        session1:get_coordinator(),
+        c1:get_coordinator(),
         {uuid = kid, uri = 'localhost:9'}
     )
 
     t.assert_equals(
-        session2:acquire_lock({uuid.str(), 'localhost:11'}),
+        c2:acquire_lock({uuid.str(), 'localhost:11'}),
         false
     )
 
-    local ok, err = session2:set_leaders({{'A', 'a1'}})
+    local ok, err = c2:set_leaders({{'A', 'a1'}})
     t.assert_equals(ok, nil)
     t.assert_equals(err, 'You are not holding the lock')
 
     t.assert_equals(
-        session2:get_coordinator(),
+        c2:get_coordinator(),
         {uuid = kid, uri = 'localhost:9'}
     )
 
-    session1:drop()
+    c1:drop()
 
     local kid = uuid.str()
     helpers.retrying({}, function()
-        t.assert_equals(session2:get_coordinator(), nil)
+        t.assert_equals(c1:get_coordinator(), nil)
     end)
 
     t.assert_equals(
-        session2:acquire_lock({kid, 'localhost:11'}),
+        c2:acquire_lock({kid, 'localhost:11'}),
         true
     )
     t.assert_equals(
-        session2:get_coordinator(),
+        c2:get_coordinator(),
         {uuid = kid, uri = 'localhost:11'}
     )
 end
 
 function g.test_appointments()
-    local session = create_client(g.stateboard):get_session()
+    local c = create_client(g.stateboard):get_session()
     local kid = uuid.str()
     t.assert_equals(
-        session:acquire_lock({kid, 'localhost:9'}), true
+        c:acquire_lock({kid, 'localhost:9'}),
+        true
     )
 
     t.assert_equals(
-        session:set_leaders({{'A', 'a1'}, {'B', 'b1'}}), true
+        c:set_leaders({{'A', 'a1'}, {'B', 'b1'}}),
+        true
     )
 
     t.assert_equals(
-        session:get_leaders(), {A = 'a1', B = 'b1'}
+        c:get_leaders(),
+        {A = 'a1', B = 'b1'}
     )
 
-    local ok, err = session:set_leaders({{'A', 'a2'}, {'A', 'a3'}})
+    local ok, err = c:set_leaders({{'A', 'a2'}, {'A', 'a3'}})
     t.assert_equals(ok, nil)
-    t.assert_equals(err.err,
-        "Duplicate key exists in unique index 'ordinal'" ..
+    t.assert_covers(err, {
+        class_name = 'NetboxCallError',
+        err = "Duplicate key exists in unique index 'ordinal'" ..
         " in space 'leader_audit'"
-    )
+    })
 end
 
 function g.test_longpolling()
-    local client1 = create_client(g.stateboard)
-    local session1 = client1:get_session()
+    local c1 = create_client(g.stateboard):get_session()
     local kid = uuid.str()
     t.assert_equals(
-        session1:acquire_lock({kid, 'localhost:9'}),
+        c1:acquire_lock({kid, 'localhost:9'}),
         true
     )
-    session1:set_leaders({{'A', 'a1'}, {'B', 'b1'}})
+    c1:set_leaders({{'A', 'a1'}, {'B', 'b1'}})
 
     local client2 = create_client(g.stateboard)
     t.assert_equals(client2:longpoll(0), {A = 'a1', B = 'b1'})
@@ -135,7 +138,7 @@ function g.test_longpolling()
     end
 
     local chan = async_longpoll()
-    session1:set_leaders({{'A', 'a2'}})
+    c1:set_leaders({{'A', 'a2'}})
     t.assert_equals(chan:get(0.1), {{A = 'a2'}, nil})
 
     local chan = async_longpoll()
@@ -177,27 +180,27 @@ function g.test_outage()
 
     local payload = {uuid.str(), 'localhost:9'}
 
-    local session1 = create_client(g.stateboard):get_session()
+    local c1 = create_client(g.stateboard):get_session()
     t.assert_equals(
-        session1:acquire_lock(payload), true
+        c1:acquire_lock(payload), true
     )
     t.assert_equals(
         -- C1 can renew expired lock if it wasn't stolen yet
-        session1:acquire_lock(payload), true
+        c1:acquire_lock(payload), true
     )
 
-    local session2 = create_client(g.stateboard):get_session()
+    local c2 = create_client(g.stateboard):get_session()
     t.assert_equals(
-        session2:acquire_lock(payload), true
+        c2:acquire_lock(payload), true
     )
-    session2:drop()
+    c2:drop()
 
     -- C1 can't renew lock after it was stolen by C2
-    local ok, err = session1:acquire_lock(payload)
+    local ok, err = c1:acquire_lock(payload)
     t.assert_equals(ok, nil)
     t.assert_equals(err, 'The lock was stolen')
 
-    local ok, err = session1:set_leaders({})
+    local ok, err = c1:set_leaders({})
     t.assert_equals(ok, nil)
     t.assert_equals(err, 'You are not holding the lock')
 end
