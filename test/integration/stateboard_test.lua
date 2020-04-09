4c01a2cdcd25c0c0fa7c33cd20b259e917003b1f
Alexandr Barulev <huston.mavr@gmail.com>
2020-04-09 12:58:50 +0300

    Merge statebord tests (#753)

--- test/integration/stateboard_test.lua
+++ test/integration/stateboard_test.lua
@@ -216,3 +216,64 @@ function g.test_outage()
         err = 'You are not holding the lock'
     })
 end
+
+function g.test_client_session()
+    -- get_session always returns alive one
+    local client = create_client(g.stateboard)
+    local session = client:get_session()
+    t.assert_equals(session:is_alive(), true)
+    t.assert_is(client:get_session(), session)
+
+    local ok = session:acquire_lock({'uuid', 'uri'})
+    t.assert_equals(ok, true)
+    t.assert_equals(session:is_alive(), true)
+    t.assert_equals(session:is_locked(), true)
+    t.assert_is(client:get_session(), session)
+
+    -- get_session creates new session if old one is dead
+    g.stateboard:stop()
+    local ok, err = session:get_leaders()
+    t.assert_equals(ok, nil)
+    t.assert_covers(err, {
+        class_name = 'NetboxCallError',
+        err = 'Connection reset by peer',
+    })
+    t.assert_is_not(client:get_session(), session)
+
+    -- session looses lock if connection is interrupded
+    t.assert_equals(session:is_alive(), false)
+    t.assert_equals(session:is_locked(), false)
+end
+
+function g.test_client_drop_session()
+    local client = create_client(g.stateboard)
+    local session = client:get_session()
+    t.assert_equals(session:is_alive(), true)
+    t.assert_is(client:get_session(), session)
+
+    local ok = session:acquire_lock({'uuid', 'uri'})
+    t.assert_equals(ok, true)
+    t.assert_equals(session:is_alive(), true)
+    t.assert_equals(session:is_locked(), true)
+    t.assert_is(client:get_session(), session)
+
+    client:drop_session()
+
+    local ok, err = session:get_leaders()
+    t.assert_equals(ok, nil)
+    t.assert_covers(err, {
+        class_name = 'NetboxCallError',
+        err = 'Connection closed',
+    })
+
+    -- dropping session releases lock and make it dead
+    t.assert_equals(session:is_alive(), false)
+    t.assert_equals(session:is_locked(), false)
+
+    -- dropping session is idempotent
+    client:drop_session()
+    t.assert_equals(session:is_alive(), false)
+    t.assert_equals(session:is_locked(), false)
+
+    t.assert_is_not(client:get_session(), session)
+end
