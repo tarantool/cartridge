405567125509859409ceff4dceea183c3a73db67
Roman Babaev <r.babaev@corp.mail.ru>
2020-04-16 02:11:19 +0300

    Update cluster list layout

--- webui/src/components/ReplicasetServerListItem.js
+++ webui/src/components/ReplicasetServerListItem.js
@@ -33,14 +33,14 @@ const styles = {
     display: flex;
     flex-wrap: wrap;
     align-items: baseline;
-    padding-right: 40px;
+    padding-right: 31px;
     margin-bottom: -8px;
   `,
   heading: css`
-    flex-basis: 430px;
+    flex-basis: 415px;
     flex-grow: 1;
     flex-shrink: 0;
-    margin-right: 24px;
+    margin-right: 16px;
     margin-bottom: 8px;
     overflow: hidden;
   `,
@@ -67,47 +67,57 @@ const styles = {
     left: 3px;
   `,
   iconMargin: css`
-    margin-right: 8px;
-  `,
-  iconMarginSmall: css`
     margin-right: 4px;
   `,
   statusGroup: css`
     display: flex;
     flex-basis: 576px;
     flex-shrink: 0;
+    flex-grow: 1;
     align-items: flex-start;
     margin-bottom: 8px;
   `,
   memStats: css`
-    width: 229px;
+    flex-shrink: 0;
+    width: 246px;
+  `,
+  memStatsRow: css`
+    display: flex;
+    align-items: center;
+  `,
+  statsText: css`
+    white-space: nowrap;
   `,
   memProgress: css`
     width: auto;
-    margin-left: 24px;
+    margin-left: 20px;
   `,
   configureBtn: css`
     position: absolute;
     top: 12px;
-    right: 16px;
+    right: 12px;
   `,
   status: css`
     flex-basis: 193px;
     flex-shrink: 0;
     margin-top: 1px;
-    margin-right: 24px;
+    margin-right: 16px;
     margin-left: -8px;
   `,
   stats: css`
-    position: relative;
+    position: absolute;
+    right: 46px;
     display: flex;
-    flex-basis: 351px;
     flex-shrink: 0;
     align-items: stretch;
+    margin-left: auto;
+    width: 384px;
   `,
   bucketsCount: css`
     flex-shrink: 0;
-    width: 120px;
+    display: flex;
+    align-items: center;
+    width: 122px;
     margin-right: 16px;
   `,
   tags: css`
@@ -237,8 +247,8 @@ class ReplicasetServerListItem extends React.PureComponent<
                           </React.Fragment>
                         )}
                       >
-                        <IconBucket className={styles.iconMarginSmall} />
-                        <Text variant='h5' tag='span'>
+                        <IconBucket className={styles.iconMargin} />
+                        <Text className={styles.statsText} variant='h5' tag='span'>
                           Buckets: <b>{(statistics && statistics.bucketsCount) || '-'}</b>
                         </Text>
                       </Tooltip>
@@ -247,8 +257,8 @@ class ReplicasetServerListItem extends React.PureComponent<
                   }
                   <div className={styles.memStats}>
                     <div>
-                      <MemoryIcon {...statistics} />
-                      <Text variant='h5' tag='span'>{usageText}</Text>
+                      <MemoryIcon {...statistics} className={styles.iconMargin} />
+                      <Text className={styles.statsText} variant='h5' tag='span'>{usageText}</Text>
                     </div>
                     <ProgressBar
                       className={styles.memProgress}
