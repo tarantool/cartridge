f401fbe6f90c498f2802f875134acf68498a53e1
Alla Khintal <feeramail@gmail.com>
2020-05-09 01:03:07 +0400

    Test for empty username in WebUI (#831)

--- webui/cypress/integration/login-and-logout.spec.js
+++ webui/cypress/integration/login-and-logout.spec.js
@@ -1,9 +1,5 @@
-//Steps:
-//1. Login error
-//2. Success login
-
-
 describe('Login', () => {
+
   it('Login error', () => {
     cy.visit(Cypress.config('baseUrl'));
     cy.get('.meta-test__LoginBtn').click();
@@ -15,6 +11,7 @@ describe('Login', () => {
     cy.get('.meta-test__LoginFormBtn').click();
     cy.get('.meta-test__LoginForm').contains('Authentication failed');//try to found logout btn
   })
+
   it('Success login', () => {
     cy.visit(Cypress.config('baseUrl'));
     cy.get('.meta-test__LoginBtn').click();
@@ -27,8 +24,39 @@ describe('Login', () => {
     cy.get('.meta-test__LoginFormBtn').click();
     cy.get('.meta-test__LogoutBtn');//try to found logout btn
   })
+
   it('Logout', () => {
     cy.get('.meta-test__LogoutBtn').click();
     cy.get('li').contains('Log out').click();
+  })
+
+  it('Check login user with empty name', () => {
+    cy.visit(Cypress.config('baseUrl') + '/admin/cluster/users');
+
+    //create user without fullname:
+    cy.get('.meta-test__addUserBtn').click();
+    cy.get('.meta-test__UserAddForm input[name="username"]').type('testuser');
+    cy.get('.meta-test__UserAddForm input[name="password"]').type('testpassword');
+    cy.get('.meta-test__UserAddForm button:contains(Add)').click();
+
+    //login user:
+    cy.get('.meta-test__LoginBtn').click();
+    cy.get('.meta-test__LoginForm input[name="username"]')
+      .type('{selectall}{del}')
+      .type('testuser');
+    cy.get('.meta-test__LoginForm input[name="password"]')
+      .type('testpassword');
+    cy.get('.meta-test__LoginFormBtn').click();
+
+    cy.get('.meta-test__LoginBtn').should('not.exist');
+    cy.get('.meta-test__LogoutBtn');
+
+    //logout and remove testuser:
+    cy.get('.meta-test__LogoutBtn').click();
+    cy.get('li').contains('Log out').click();
+
+    cy.get('.meta-test__UsersTable li:contains(testuser)').find('button').click();
+    cy.get('li').contains('Remove user').click();
+    cy.get('.meta-test__UserRemoveModal button[type="button"]:contains(Remove)').click();
   })
 });
