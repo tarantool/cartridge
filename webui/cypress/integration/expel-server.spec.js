728e7b46189243888200fc3da797cda4a7cbb2c2
Alla <feeramail@gmail.com>
2020-05-28 20:43:19 +0300

    Correction of non-working tests

--- webui/cypress/integration/expel-server.spec.js
+++ webui/cypress/integration/expel-server.spec.js
@@ -3,7 +3,7 @@ describe('Expel server', () => {
   it('Expel server', () => {
     cy.visit(Cypress.config('baseUrl'));
     cy.get('li').contains('storage1-do-not-use-me').closest('li').find('.meta-test__ReplicasetServerListItem__dropdownBtn').eq(1).click();
-    cy.get('.meta-test__ReplicasetServerListItem__dropdown').contains('Expel server').click();
+    cy.get('li').contains('Expel server').click();
     cy.get('.meta-test__ExpelServerModal button[type="button"]').contains('Expel').click();
 
     cy.get('span:contains(Expel is OK. Please wait for list refresh...)').click();
@@ -12,7 +12,7 @@ describe('Expel server', () => {
   it('Show expel error', () => {
     cy.reload();
     cy.get('li').contains('storage1-do-not-use-me').closest('li').find('.meta-test__ReplicasetServerListItem__dropdownBtn').eq(0).click();
-    cy.get('.meta-test__ReplicasetServerListItem__dropdown').contains('Expel server').click();
+    cy.get('li').contains('Expel server').click();
     cy.get('.meta-test__ExpelServerModal button[type="button"]').contains('Expel').click();
     cy.get('.meta-test__ExpelServerModal button[type="button"]').contains('Cancel').click();
     cy.get('span:contains(An error has occurred)').click();
