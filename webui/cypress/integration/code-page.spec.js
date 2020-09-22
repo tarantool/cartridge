1d6bb90cb96cca6e446a9da72de66043fd0b4631
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2020-09-22 19:22:53 +0300

    Cleanup code-page tests

--- webui/cypress/integration/code-page.spec.js
+++ webui/cypress/integration/code-page.spec.js
@@ -1,28 +1,28 @@
 describe('Code page', () => {
 
   before(() => {
-    cy.task('tarantool', {
-      code: `
-        cleanup()
+    cy.task('tarantool', {code: `
+      cleanup()
 
-        local workdir = fio.tempdir()
-        fio.mktree(workdir)
-        _G.cluster = helpers.Cluster:new({
-          datadir = fio.tempdir(),
-          server_command = helpers.entrypoint('srv_basic'),
-          use_vshard = true,
-          cookie = helpers.random_cookie(),
-          replicasets = {{
-            uuid = helpers.uuid('a'),
-            alias = 'a',
-            roles = {'vshard-router', 'vshard-storage', 'failover-coordinator'},
-            servers = {{http_port = 8080}, {}},
-          }}
-        })
-  
-        _G.cluster:start()
-        return true
-      `}).should('deep.eq', [true]);
+      _G.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        server_command = helpers.entrypoint('srv_basic'),
+        use_vshard = true,
+        cookie = helpers.random_cookie(),
+        replicasets = {{
+          uuid = helpers.uuid('a'),
+          alias = 'dummy',
+          roles = {'vshard-router', 'vshard-storage', 'failover-coordinator'},
+          servers = {{http_port = 8080}, {}},
+        }}
+      })
+
+      for _, srv in pairs(_G.cluster.servers) do
+        srv.env.TARANTOOL_INSTANCE_NAME = srv.alias
+      end
+      _G.cluster:start()
+      return true
+    `}).should('deep.eq', [true]);
   });
 
   after(() => {
@@ -30,24 +30,24 @@ describe('Code page', () => {
   });
 
   const selectAllKeys = Cypress.platform == 'darwin' ? '{cmd}a' : '{ctrl}a';
-  
+
   function reload() {
-        cy.get('.meta-test__Code__reload_idle').click({force: true});
-        cy.get('button[type="button"]').contains('Ok').click();
-        cy.get('.meta-test__Code__reload_loading').should('not.exist');
+    cy.get('.meta-test__Code__reload_idle').click({force: true});
+    cy.get('button[type="button"]').contains('Ok').click();
+    cy.get('.meta-test__Code__reload_loading').should('not.exist');
+    cy.get('.meta-test__Code__reload_idle').should('exist');
   }
   function apply() {
-        cy.get('.meta-test__Code__apply_idle').click({force: true});
-        cy.get('.meta-test__Code__apply_loading').should('not.exist');
+    cy.get('.meta-test__Code__apply_idle').click({force: true});
+    cy.get('.meta-test__Code__apply_loading').should('not.exist');
+    cy.get('.meta-test__Code__apply_idle').should('exist');
   }
 
   it('Open WebUI', () => {
-    cy.visit('/admin/cluster/code')
+    cy.visit('/admin/cluster/code');
+    cy.get('.meta-test__Code__apply_idle').should('exist');
+    cy.title().should('eq', 'dummy-1: Code');
   });
-
-  it('Tab title on Code page', () => {
-    cy.title().should('eq', 'a-1: Code')
-  })
 
   it('Empty code page', () => {
     cy.get('#root').contains('Please select a file');
@@ -56,23 +56,23 @@ describe('Code page', () => {
   })
 
   it('File in tree', () => {
-    //create file and file contents
     cy.get('.meta-test__addFileBtn').click();
     cy.get('.meta-test__enterName').focused().type('file-in-tree1.yml');
-    cy.get('#root').contains('a-1').click();
+    cy.get('#root').contains('dummy-1').click();
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree1.yml');
 
-    //reload
-    reload()
+    reload();
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree1.yml').should('not.exist');
 
-    //apply
     cy.get('.meta-test__addFileBtn').click();
     cy.get('.meta-test__enterName').focused().type('file-in-tree2.yml{enter}');
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree2.yml');
-    apply()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree2.yml');
-    reload()
+    reload();
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree2.yml');
 
     //file contents
@@ -85,22 +85,27 @@ describe('Code page', () => {
     cy.get('a[href="/admin/cluster/code"]').click();
     cy.get('.monaco-editor textarea').should('have.value', 'some test code');
 
-    reload()
-    cy.wait(1000)
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', '');
 
     cy.get('.ScrollbarsCustom-Content').contains('file-in-tree2.yml').click();
     cy.get('.monaco-editor textarea').type('some test code2');
-    apply()
-    reload()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', 'some test code2');
 
     // wrong yaml error
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
     cy.get('.monaco-editor textarea').type('some: [] test code2');
     apply()
-    cy.get('#root').contains('LoadConfigError: Error parsing section "file-in-tree2.yml": did not find expected key at document');
-    reload()
+    cy.get('#root').contains(
+      'LoadConfigError: Error parsing section "file-in-tree2.yml":' +
+      ' did not find expected key at document'
+    ).click();
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', 'some test code2');
 
     //edit file and file contents
@@ -115,8 +120,10 @@ describe('Code page', () => {
     //apply
     cy.get('.meta-test__editFolderInTreeBtn').eq(0).click({ force: true });
     cy.get('.meta-test__enterName').focused().clear().type('edited-file-name2{enter}');
-    apply()
-    reload()
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
+    reload();
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2').click();
 
     //file contents
@@ -125,14 +132,17 @@ describe('Code page', () => {
     cy.get('.monaco-editor textarea').type('edit test code');
     cy.get('.monaco-editor textarea').should('have.value', 'edit test code');
 
-    reload()
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', 'some test code2');
 
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2').click();
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
     cy.get('.monaco-editor textarea').type('edit test code2');
-    apply()
-    reload()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', 'edit test code2');
 
     //delete file and file contents
@@ -142,36 +152,41 @@ describe('Code page', () => {
     cy.get('.monaco-editor textarea').type('a{backspace}'); //без этого не работает
     cy.get('.monaco-editor textarea').should('have.value', '');
 
-    reload()
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', 'edit test code2');
 
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2').click();
     cy.get('.monaco-editor textarea').type(selectAllKeys + '{backspace}');
-    apply()
-    reload()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
+    reload();
     cy.get('.monaco-editor textarea').should('have.value', '');
 
-    //reload
     cy.get('.meta-test__deleteFolderInTreeBtn').eq(0).click({ force: true });
     cy.get('.meta-test__deleteModal button[type="button"]').contains('Ok').click();
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2').should('not.exist');
-    reload()
+
+    reload();
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2');
 
-    //apply
     cy.get('.meta-test__deleteFolderInTreeBtn').eq(0).click({ force: true });
     cy.get('.meta-test__deleteModal').should('be.visible');
     cy.get('.meta-test__deleteModal button[type="button"]').contains('Ok').click();
-    apply()
-    reload()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
+    reload();
     cy.get('.ScrollbarsCustom-Content').contains('edited-file-name2').should('not.exist');
-  })
+  });
 
   it('Folder in tree', () => {
     //create folder
     cy.get('.meta-test__addFolderBtn').click();
     cy.get('.meta-test__enterName').focused().type('folder-in-tree');
-    cy.get('#root').contains('a-1').click();
+    cy.get('#root').contains('dummy-1').click();
     cy.get('.meta-test__Code__FileTree').contains('folder-in-tree');
 
     //create folder in folder
@@ -193,7 +208,9 @@ describe('Code page', () => {
     cy.get('.meta-test__Code__FileTree').contains('file-in-folder').should('be.visible');
 
     //save changes and full reload code page
-    apply()
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
     cy.reload();
     cy.contains('Not loaded').should('not.exist');
     cy.get('.meta-test__Code__FileTree').contains('folder-in-folder').should('not.exist');
@@ -205,7 +222,10 @@ describe('Code page', () => {
     cy.get('.meta-test__deleteFolderInTreeBtn').eq(0).click({ force: true });
     cy.get('.meta-test__deleteModal').should('be.visible');
     cy.get('.meta-test__deleteModal button[type="button"]').contains('Ok').click();
-    apply()
+
+    apply();
+    cy.get('span:contains(Success) + span:contains(Files successfuly applied)').click();
+
     cy.get('.meta-test__Code__FileTree').contains('edited-folder-name').should('not.exist');
   })
 
