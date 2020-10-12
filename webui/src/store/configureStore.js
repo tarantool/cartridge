04d708251d640c5cd6b6fc9356c1e59c88ddf535
Roman Babaev <kotfly@gmail.com>
2020-10-12 17:32:10 +0300

    Removes redux-logger from webui (#1089)
    Co-authored-by: Roman Babaev <r.babaev@corp.mail.ru>

--- webui/src/store/configureStore.js
+++ webui/src/store/configureStore.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger as createLoggerMiddleware } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './rootReducer';
@@ -8,13 +7,12 @@ import rootSaga from './rootSaga';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const sagaMiddleware = createSagaMiddleware();
-const loggerMiddleware = createLoggerMiddleware({ collapsed: true });
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(sagaMiddleware, loggerMiddleware))
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   );
 
   sagaMiddleware.run(rootSaga);
