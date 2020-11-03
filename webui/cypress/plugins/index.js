c1f2515599bd8cbaf2b790fc8fbd92bab374600c
Roman Babaev <r.babaev@corp.mail.ru>
2020-11-03 18:13:52 +0300

    Test upd

--- webui/cypress/plugins/index.js
+++ webui/cypress/plugins/index.js
@@ -20,67 +20,67 @@ const yaml = require('js-yaml');
 const cache = {};
 
 function connect(host, port) {
-    return new Promise((resolve, reject) => {
-        const addr = host + ':' + port;
+  return new Promise((resolve, reject) => {
+    const addr = host + ':' + port;
 
-        if (cache[addr] && !cache[addr].destroyed) {
-            resolve(cache[addr]);
-            return;
-        }
+    if (cache[addr] && !cache[addr].destroyed) {
+      resolve(cache[addr]);
+      return;
+    }
 
-        const conn = net.connect(port, host);
-        cache[addr] = conn;
-        conn.setEncoding('utf8');
+    const conn = net.connect(port, host);
+    cache[addr] = conn;
+    conn.setEncoding('utf8');
 
-        // Fetch the greeting
-        const _on_data = (data) => {
-            conn.off('data', _on_data);
-            resolve(conn);
-        }
+    // Fetch the greeting
+    const _on_data = data => {
+      conn.off('data', _on_data);
+      resolve(conn);
+    }
 
-        const _on_error = (error) => {
-            conn.off('error', _on_error);
-            reject(error);
-        }
+    const _on_error = error => {
+      conn.off('error', _on_error);
+      reject(error);
+    }
 
-        conn.on('data', _on_data);
-        conn.on('error', _on_error);
-    });
+    conn.on('data', _on_data);
+    conn.on('error', _on_error);
+  });
 }
 
 function communicate(conn, text) {
-    return new Promise((resolve) => {
-        conn.write(text);
-        const chunks = [];
+  return new Promise(resolve => {
+    conn.write(text);
+    const chunks = [];
 
-        const _on_data = (data) => {
-            chunks.push(data);
-            if (data.endsWith('\n...\n')) {
-                conn.off('data', _on_data);
-                const resp = chunks.join('')
-                resolve(resp);
-            }
-        }
-        conn.on('data', _on_data);
-    })
+    const _on_data = data => {
+      chunks.push(data);
+      if (data.endsWith('\n...\n')) {
+        conn.off('data', _on_data);
+        const resp = chunks.join('')
+        resolve(resp);
+      }
+    }
+    conn.on('data', _on_data);
+  })
 }
 
 module.exports = (on, config) => {
-    on('task', {
-        async tarantool({
-            host = config.env.launcherHost,
-            port = config.env.launcherPort,
-            code
-        }) {
-            const conn = await connect(host, port);
-            const fcmd = code.split('\n').join(' ') + '\n';
-            const resp = yaml.safeLoad(await communicate(conn, fcmd));
+  on('task', {
+    async tarantool({
+      host = config.env.launcherHost,
+      port = config.env.launcherPort,
+      code
+    }) {
+      const conn = await connect(host, port);
+      const fcmd = code.split('\n').join(' ') + '\n';
+      const resp = yaml.safeLoad(await communicate(conn, fcmd));
 
-            if (resp && resp[0] && resp[0].error) {
-                throw new Error(resp[0].error)
-            }
+      if (resp && resp[0] && resp[0].error) {
+        throw new Error(resp[0].error)
+      }
 
-            return resp;
-        }
-    })
+      return resp;
+    }
+  })
 }
