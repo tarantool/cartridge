73934ca62af49dead561b05773147c515f0951ad
Aleksandr.Shemenev <Shemeneval@gmail.com>
2020-12-09 22:37:11 +0300

    Implement cartridge.config_force_apply (#1167)
    New API will heal several operational errors:
    
    - "Prepare2pcError: Two-phase commit is locked";
    - "SaveConfigError: .../config.prepare: Directory not empty";
    - "Configuration is prepared and locked on ..." (an issue);
    - "Configuration checksum mismatch on ..." (an issue).
    
    It'll unlock two-phase commit (remove `config.prepare` lock), upload the
    active config from the current instance and reconfigure all roles.
    
    Part of #1144

--- cartridge/twophase.lua
+++ cartridge/twophase.lua
@@ -23,6 +23,7 @@ local AtomicCallError = errors.new_class('AtomicCallError')
 local PatchClusterwideError = errors.new_class('PatchClusterwideError')
 local Prepare2pcError = errors.new_class('Prepare2pcError')
 local Commit2pcError = errors.new_class('Commit2pcError')
+local ForceReapplyError = errors.new_class('ForceReapplyError')
 local GetSchemaError = errors.new_class('GetSchemaError')
 
 yaml.cfg({
@@ -78,6 +79,10 @@ local function prepare_2pc(data)
     end
 
     local ok, err = ClusterwideConfig.save(clusterwide_config, path_prepare)
+    if not ok and fio.path.exists(path_prepare) then
+        err = Prepare2pcError:new('Two-phase commit is locked')
+    end
+
     if not ok then
         log.warn('%s', err)
         return nil, err
@@ -165,6 +170,73 @@ local function abort_2pc()
     return true
 end
 
+local function reapply(data)
+    local clusterwide_config = ClusterwideConfig.new(data):lock()
+    vars.prepared_config = clusterwide_config -- lock before wishing state
+
+    local state = confapplier.wish_state('RolesConfigured')
+    if state ~= 'Unconfigured'
+    and state ~= 'RolesConfigured'
+    and state ~= 'OperationError'
+    then
+        local err = ForceReapplyError:new(
+            "Instance state is %s, can't reapply config in this state",
+            state
+        )
+        log.warn('%s', err)
+        return nil, err
+    end
+
+    local workdir = confapplier.get_workdir()
+    local path_prepare = fio.pathjoin(workdir, 'config.prepare')
+    local path_backup = fio.pathjoin(workdir, 'config.backup')
+    local path_active = fio.pathjoin(workdir, 'config')
+    ClusterwideConfig.remove(path_prepare)
+
+    local ok, err = ClusterwideConfig.save(clusterwide_config, path_prepare)
+    if not ok then
+        log.warn('%s', err)
+        return nil, err
+    end
+
+    ClusterwideConfig.remove(path_backup)
+
+    if fio.path.exists(path_active) then
+        local ok = fio.rename(path_active, path_backup)
+        if ok then
+            log.info('Backup of active config created: %q', path_backup)
+        else
+            log.warn('Creation of config backup failed: %s', errno.strerror())
+        end
+    end
+
+    vars.prepared_config = nil
+
+    local ok = fio.rename(path_prepare, path_active)
+    if not ok then
+        local err = ForceReapplyError:new(
+            "Can't move %q: %s", path_prepare, errno.strerror()
+        )
+        log.error('%s', err)
+        return nil, err
+    end
+
+    local state = confapplier.wish_state('RolesConfigured')
+
+    if state == 'Unconfigured' then
+        return confapplier.boot_instance(clusterwide_config)
+    elseif state == 'RolesConfigured' or state == 'OperationError' then
+        return confapplier.apply_config(clusterwide_config)
+    else
+        local err = ForceReapplyError:new(
+            "Instance state is %s, can't reapply config in this state",
+            state
+        )
+        log.error('%s', err)
+        return nil, err
+    end
+end
+
 --- Edit the clusterwide configuration.
 -- Top-level keys are merged with the current configuration.
 -- To remove a top-level section, use
@@ -247,7 +319,6 @@ local function _clusterwide(patch)
         return nil, err
     end
     clusterwide_config_new:lock()
-    -- log.info('%s', yaml.encode(clusterwide_config_new:get_readonly()))
 
     local topology_old = clusterwide_config_old:get_readonly('topology')
     local topology_new = clusterwide_config_new:get_readonly('topology')
@@ -304,7 +375,8 @@ local function _clusterwide(patch)
         log.warn('(2PC) Preparation stage...')
 
         local retmap, errmap = pool.map_call(
-            '_G.__cartridge_clusterwide_config_prepare_2pc', {clusterwide_config_new:get_plaintext()},
+            '_G.__cartridge_clusterwide_config_prepare_2pc',
+            {clusterwide_config_new:get_plaintext()},
             {uri_list = uri_list, timeout = 5}
         )
 
@@ -408,6 +480,92 @@ local function patch_clusterwide(patch)
 end
 
 
+--- Forcefully apply config to the given instances.
+--
+-- In particular:
+--
+-- - Abort two-phase commit (remove `config.prepare` lock)
+-- - Upload the active config from the current instance.
+-- - Apply it (reconfigure all roles)
+--
+-- (**Added** in v2.3.0-68)
+--
+-- @function force_reapply
+-- @tparam {string,...} uuids
+-- @treturn[1] boolean true
+-- @treturn[2] nil
+-- @treturn[2] table Error description
+local function _force_reapply(uuids)
+    checks('table')
+
+    local clusterwide_config = confapplier.get_active_config()
+    local current_topology = clusterwide_config:get_readonly('topology')
+
+    local _reapply_error
+
+    -- Prepare a server group to be configured
+    local uri_list = {}
+    local refined_uri_list = topology.refine_servers_uri(current_topology)
+    for _, uuid, _ in fun.filter(topology.not_disabled, current_topology.servers) do
+        if utils.table_find(uuids, uuid) then
+            table.insert(uri_list, refined_uri_list[uuid])
+        end
+    end
+
+    do
+        log.warn('Reapplying clusterwide config forcefully...')
+
+        local retmap, errmap = pool.map_call(
+            '_G.__cartridge_clusterwide_config_reapply',
+            {clusterwide_config:get_plaintext()},
+            {uri_list = uri_list, timeout = 5}
+        )
+
+        for _, uri in ipairs(uri_list) do
+            if retmap[uri] then
+                log.warn('Reapplied config forcefully at %s', uri)
+            end
+        end
+        for _, uri in ipairs(uri_list) do
+            if retmap[uri] == nil then
+                local err = errmap and errmap[uri]
+                if err == nil then
+                    err = ForceReapplyError:new('Unknown error at %s', uri)
+                end
+                log.error('Error reappling config at %s:\n%s', uri, err)
+                _reapply_error = err
+            end
+        end
+
+    end
+
+    if _reapply_error == nil then
+        log.warn('Clusterwide config reapplied successfully')
+        return true
+    else
+        log.error('Clusterwide config reapply failed')
+        return nil, _reapply_error
+    end
+end
+
+local function force_reapply(uuids)
+    if vars.locks['clusterwide'] == true  then
+        return nil, AtomicCallError:new(
+            'cartridge.patch_clusterwide is already running'
+        )
+    end
+
+    vars.locks['clusterwide'] = true
+    local ok, err = PatchClusterwideError:pcall(_force_reapply, uuids)
+    vars.locks['clusterwide'] = false
+
+    if not ok then
+        return nil, err
+    end
+
+    return true
+end
+
 --- Get clusterwide DDL schema.
 --
 -- (**Added** in v1.2.0-28)
@@ -493,6 +651,8 @@ end
 _G.__cartridge_clusterwide_config_prepare_2pc = function(...) return errors.pcall('E', prepare_2pc, ...) end
 _G.__cartridge_clusterwide_config_commit_2pc = function(...) return errors.pcall('E', commit_2pc, ...) end
 _G.__cartridge_clusterwide_config_abort_2pc = function(...) return errors.pcall('E', abort_2pc, ...) end
+_G.__cartridge_clusterwide_config_reapply = function(...) return errors.pcall('E', reapply, ...) end
+
 
 -- Keep backward compatibility with the good old cartridge 1.2.0.
 _G.__cluster_confapplier_prepare_2pc = function(conf)
@@ -516,4 +676,5 @@ return {
     get_schema = get_schema,
     set_schema = set_schema,
     patch_clusterwide = patch_clusterwide,
+    force_reapply = force_reapply,
 }
---
+++ test/integration/force_reapply_test.lua
@@ -0,0 +1,90 @@
+local t = require('luatest')
+local g = t.group()
+
+local fio = require('fio')
+local helpers = require('test.helper')
+
+g.before_all(function()
+    g.cluster = helpers.Cluster:new({
+        datadir = fio.tempdir(),
+        use_vshard = false,
+        server_command = helpers.entrypoint('srv_basic'),
+        cookie = require('digest').urandom(6):hex(),
+        replicasets = {{
+            alias = 'A',
+            roles = {},
+            servers = 3,
+        }},
+    })
+    g.cluster:start()
+    g.A1 = g.cluster:server('A-1')
+    g.A2 = g.cluster:server('A-2')
+    g.A3 = g.cluster:server('A-3')
+    t.helpers.retrying({}, function()
+        t.assert_equals(helpers.list_cluster_issues(g.A1), {})
+    end)
+end)
+
+g.after_all(function()
+    g.cluster:stop()
+    fio.rmtree(g.cluster.datadir)
+end)
+
+
+function g.test_twophase_config_locked()
+    -- Let's put a spoke in two-phases wheel
+    local config = g.A1.net_box:eval([[
+        local confapplier = require('cartridge.confapplier')
+        return confapplier.get_active_config():get_plaintext()
+    ]])
+    config['hey.txt'] = 'Hello, locks'
+    g.A2.net_box:call(
+        '_G.__cartridge_clusterwide_config_prepare_2pc', {config}
+    )
+
+    t.assert_equals(fio.path.exists(g.A1.workdir .. '/config.prepare'), false)
+    t.assert_equals(fio.path.exists(g.A2.workdir .. '/config.prepare'), true)
+    t.assert_equals(fio.path.exists(g.A3.workdir .. '/config.prepare'), false)
+    t.assert_equals(helpers.list_cluster_issues(g.A1), {{
+        level = 'warning',
+        topic = 'configuration',
+        instance_uuid = g.A2.instance_uuid,
+        replicaset_uuid = g.A2.replicaset_uuid,
+        message = 'Configuration is prepared and locked'..
+            ' on localhost:13302 (A-2)',
+    }})
+
+    -- Obviously, 2pc is locked
+    local ok, err = g.A1.net_box:call(
+        'package.loaded.cartridge.config_patch_clusterwide',
+        {{['bye.txt'] = 'Goodbye, locks'}}
+    )
+    t.assert_equals(ok, nil)
+    t.assert_covers(err, {
+        class_name = 'Prepare2pcError',
+        err = 'Two-phase commit is locked',
+    })
+
+    -- But force reapply comes to the rescue!
+    local ok, err = g.A1.net_box:call(
+        'package.loaded.cartridge.config_force_reapply',
+        {{g.A2.instance_uuid}}
+    )
+    t.assert_equals({ok, err}, {true, nil})
+
+    -- The lock is gone
+    t.assert_equals(fio.path.exists(g.A2.workdir .. '/config.prepare'), false)
+
+    -- And patch_clusterwide succeeds
+    local ok, err = g.A1.net_box:call(
+        'package.loaded.cartridge.config_patch_clusterwide',
+        {{['bye.txt'] = 'Goodbye, locks'}}
+    )
+    t.assert_equals({ok, err}, {true, nil})
+
+    t.assert_equals(helpers.list_cluster_issues(g.A1), {})
+    t.assert_equals(
+        g.cluster:download_config(),
+        {['bye.txt'] = 'Goodbye, locks'}
+    )
+end
