c6153cd603650b62e35e7b758d71d71475243adb
Roman Babaev <r.babaev@corp.mail.ru>
2020-12-18 17:07:29 +0300

    Remove another unused files

--- webui/src/components/Link/index.js
+++
@@ -1,3 +0,0 @@
-import { Link } from 'react-router-dom';
-
-export default Link;
--- webui/src/misc/cn.js
+++
@@ -1,7 +0,0 @@
-export function classNameFilter(name) {
-  return name != null && typeof name !== 'boolean' && name !== '';
-}
-
-export default function cn(...names) {
-  return names.filter(classNameFilter).join(' ');
-}
