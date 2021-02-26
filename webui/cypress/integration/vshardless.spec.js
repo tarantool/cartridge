c699bd7f11c651a5a74d474658dbdcf9b38d0cb6
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-02-26 18:44:59 +0300

    Fix tests

--- webui/cypress/integration/vshardless.spec.js
+++ webui/cypress/integration/vshardless.spec.js
@@ -4,7 +4,7 @@ describe('Vshardless', () => {
     cy.task('tarantool', {
       code: `
       cleanup()
-      
+
       _G.cluster = helpers.Cluster:new({
         datadir = fio.tempdir(),
         server_command = helpers.entrypoint('srv_vshardless'),
@@ -18,28 +18,16 @@ describe('Vshardless', () => {
       })
 
       for _, server in ipairs(_G.cluster.servers) do
-        server.env.TARANTOOL_INSTANCE_NAME = server.alias
-        server.env.TARANTOOL_CONSOLE_SOCK =
-          _G.cluster.datadir .. '/' .. server.alias .. '.control'
         server:start()
       end
 
       helpers.retrying({}, function()
-        _G.cluster:server('test-replicaset-1'):graphql({query = '{}'})
+        _G.cluster:server('test-replicaset-1'):graphql({query = '{ servers { uri } }'})
       end)
 
-      return _G.cluster:server('test-replicaset-1').env.TARANTOOL_CONSOLE_SOCK
+      return true
     `
-    }).then((resp) => {
-      const sock = resp[0];
-      expect(sock).to.be.a('string');
-      cy.task('tarantool', {
-        host: 'unix/', port: sock, code: `
-        package.loaded.mymodule.implies_router = true
-        package.loaded.mymodule.implies_storage = true
-        return true
-      `}).should('deep.eq', [true]);
-    });
+    }).should('deep.eq', [true]);
   });
 
   after(() => {
@@ -54,29 +42,35 @@ describe('Vshardless', () => {
   it('Vshardless: Configure Server Modal', () => {
     cy.get('.meta-test__configureBtn').first().click();
     cy.get('.meta-test__CreateReplicaSetBtn').should('be.enabled');
+    cy.get('form input[name="alias"]').type('test-replicaset');
     cy.get('form input[value="vshard-storage"]').should('not.exist');
     cy.get('form input[name="weight"]').should('be.disabled');
-    // cy.get('.meta-test__ConfigureServerModal').contains('Vshard group').should('not.exist'); //not sure
     cy.get('form input[value="default"]').should('not.exist');
     //checks all available roles
     cy.get('button[type="button"]').contains('Select all').click();
     cy.get('.meta-test__CreateReplicaSetBtn').should('be.enabled');
+
     cy.get('.meta-test__CreateReplicaSetBtn').click();
-    cy.get('.meta-test__ReplicasetServerList').should('contain', 'failover-coordinator | myrole');
     cy.get('.meta-test__configureBtn').should('not.exist');
+    cy.get('li').contains('failover-coordinator | myrole')
   })
 
   it('Vshardless: Edit Replicaset Modal', () => {
-    cy.get('#root').contains('unnamed').closest('li').find('button').contains('Edit').click();
+    cy.get('li').contains('failover-coordinator | myrole').closest('li')
+      .find('button').contains('Edit').click();
     cy.get('.meta-test__EditReplicasetSaveBtn').should('be.enabled');
     cy.get('form input[value="vshard-storage"]').should('not.exist');
     cy.get('form input[name="weight"]').should('be.disabled');
-    // cy.get('.meta-test__ConfigureServerModal').contains('Vshard group').should('not.exist'); //not sure
     cy.get('form input[value="default"]').should('not.exist');
     //checks all available roles
     cy.get('button[type="button"]').contains('Deselect all').click();
     cy.get('.meta-test__EditReplicasetSaveBtn').should('be.enabled');
     cy.get('.meta-test__EditReplicasetSaveBtn').click();
-    cy.get('.meta-test__ReplicasetServerList').should('not.contain', 'failover-coordinator | myrole');
+    cy.get('span:contains(Successful) + span:contains(Edit is OK. Please wait for list refresh...)').click();
+    cy.get('li').should('not.contain', 'failover-coordinator | myrole');
+  })
+
+  it('Vshardless: Bootstrap Button', () => {
+    cy.get('.meta-test__BootstrapButton').should('not.exist');
   })
 })
