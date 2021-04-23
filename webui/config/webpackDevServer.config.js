be550d619bc3f4098a5010499760650a7f7202dc
Roman Babaev <r.babaev@corp.mail.ru>
2021-04-23 12:40:34 +0300

    Updates frontend-core, react-dev-utils

--- webui/config/webpackDevServer.config.js
+++ webui/config/webpackDevServer.config.js
@@ -49,6 +49,7 @@ module.exports = function(proxy, allowedHost) {
     // for some reason broken when imported through Webpack. If you just want to
     // use an image, put it in `src` and `import` it from JavaScript instead.
     contentBase: paths.appPublic,
+    injectClient: false,
     // By default files from `contentBase` will not trigger a page reload.
     watchContentBase: true,
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
@@ -81,6 +82,7 @@ module.exports = function(proxy, allowedHost) {
     },
     public: allowedHost,
     proxy,
+    transportMode: 'ws',
     before(app) {
       // This lets us open files from the runtime error overlay.
       app.use(errorOverlayMiddleware());
