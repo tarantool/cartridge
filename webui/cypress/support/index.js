0333976a140feb340ff916ce135e14589876bba4
Olga Yakovleva <olga.yakovleva.github@mail.ru>
2021-07-19 13:47:27 +0500

    Update cypress

--- webui/cypress/support/index.js
+++ webui/cypress/support/index.js
@@ -1,7 +1,7 @@
-// *******************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
 //
+// // // This example support/index.js is processed and
+// // // loaded automatically before your test files.
+// // //*******************************************
 // This is a great place to put global configuration and
 // behavior that modifies Cypress.
 // You can change the location of this file or turn off
