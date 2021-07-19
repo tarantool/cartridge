e0a1ec9873bc1552af9b5c74e2656dd75020794b
Olga Yakovleva <olga.yakovleva.github@mail.ru>
2021-07-19 12:27:08 +0500

    Update cypress

--- webui/cypress/support/index.js
+++ webui/cypress/support/index.js
@@ -15,6 +15,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+//Import cypress-file-upload
+import 'cypress-file-upload';
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
