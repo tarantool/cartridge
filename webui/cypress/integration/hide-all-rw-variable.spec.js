f4add2296193f7b2927bd3bfe1d96c8486baa434
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-09-01 01:09:01 +0300

    Minimize the test

--- webui/cypress/integration/hide-all-rw-variable.spec.js
+++ webui/cypress/integration/hide-all-rw-variable.spec.js
@@ -14,133 +14,102 @@ describe('Test the cartridge_hide_all_rw frontend core variable', () => {
           uuid = helpers.uuid('a'),
           alias = 'dummy',
           roles = {},
-          servers = {{http_port = 8080}, {}, {}},
+          servers = {{http_port = 8080}},
         }}
       })
 
-      for _, server in ipairs(_G.cluster.servers) do
-        server.env.TARANTOOL_INSTANCE_NAME = server.alias
-        server.env.TARANTOOL_CONSOLE_SOCK =
-          _G.cluster.datadir .. '/' .. server.alias .. '.control'
-        server:start()
-      end
+      local server = _G.cluster:server('dummy-1')
+      server.env.TARANTOOL_CONSOLE_SOCK =
+        _G.cluster.datadir .. '/' .. server.alias .. '.control'
+      server:start()
 
       return true
     `
     }).should('deep.eq', [true]);
   });
 
+  beforeEach(() => {
+    cy.task('tarantool', {
+      code: `
+      local server = _G.cluster:server('dummy-1')
+      return {
+        sock = server.env.TARANTOOL_CONSOLE_SOCK,
+        advertise_uri = server.advertise_uri,
+        replicaset_uuid = server.replicaset_uuid,
+      }
+    `
+    }).then(([ret]) => {
+      expect(ret.sock).to.be.a('string');
+      expect(ret.advertise_uri).to.be.a('string');
+      expect(ret.replicaset_uuid).to.be.a('string');
+      cy.wrap(ret).as('main_server');
+    });
+  })
+
   after(() => {
     cy.task('tarantool', { code: `cleanup()` });
   });
 
-  it('Test: cartridge_hide_all_rw is unset', () => {
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open WebUI');
-    cy.visit('/admin/cluster/dashboard');
-    cy.title().should('eq', 'dummy-1: Cluster');
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open configure server dialog');
-    cy.get('.meta-test__UnconfiguredServerList .meta-test__configureBtn').first().click();
-    cy.get('.meta-test__ConfigureServerModal input[name="all_rw"]').first().should('exist');
-
-    cy.log('Create replica set');
-    cy.get('.meta-test__ConfigureServerModal form input[name="alias"]')
-      .should('be.focused')
-      .type('replica-name');
-
-    cy.get('.meta-test__CreateReplicaSetBtn').click();
-    cy.get('.meta-test__ConfigureServerModal').should('not.exist');
-    cy.get('#root').contains('replica-name');
-    ////////////////////////////////////////////////////////////////
-    cy.log('Open edit replica set dialog');
-    cy.get('[data-cy=meta-test__replicaSetSection] [data-cy="meta-test__editBtn"]').first().click();
-    cy.get('.meta-test__EditReplicasetModal input[name="all_rw"]').first().should('exist');
-    cy.get('.meta-test__EditReplicasetModal h2~svg').first().click(); // click close icon
-    cy.get('.meta-test__EditReplicasetModal').should('not.exist');
-  });
-
-  it('Test: cartridge_hide_all_rw is FALSE', () => {
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Setup cartridge_hide_all_rw=false');
-    cy.task('tarantool', {
-      code: `
-      _G.cluster:server('dummy-1').env.TARANTOOL_CONSOLE_SOCK
-    `
-    }).then(([sock]) => {
-      expect(sock).to.be.a('string');
+  function hideAllRW(value) {
+    cy.get('@main_server').then(srv => {
       cy.task('tarantool', {
-        host: 'unix/', port: sock, code: `
+        host: 'unix/', port: srv.sock, code: `
         local frontend = package.loaded['frontend-core']
-        frontend.set_variable('cartridge_hide_all_rw', false)
+        frontend.set_variable("cartridge_hide_all_rw", ${value})
         return true
       `
       }).should('deep.eq', [true]);
-    });
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open WebUI');
-    cy.visit('/admin/cluster/dashboard');
-    cy.title().should('eq', 'dummy-1: Cluster');
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open configure server dialog');
-    cy.get('.meta-test__UnconfiguredServerList .meta-test__configureBtn').first().click();
-    cy.get('.meta-test__ConfigureServerModal input[name="all_rw"]').first().should('exist');
+    })
+  }
 
-    cy.log('Create replica set');
-    cy.get('.meta-test__ConfigureServerModal form input[name="alias"]')
-      .should('be.focused')
-      .type('replica-name');
+  it('Test CreateReplicasetForm', function () {
+    ////////////////////////////////////////////////////////////////////
+    cy.log('cartridge_hide_all_rw is unset');
+    hideAllRW('nil');
+    cy.visit(`/admin/cluster/dashboard?s=${this.main_server.advertise_uri}`);
 
-    cy.get('.meta-test__CreateReplicaSetBtn').click();
-    cy.get('#root').contains('replica-name');
-    cy.get('.meta-test__ConfigureServerModal').should('not.exist');
-    ////////////////////////////////////////////////////////////////
-    cy.log('Open edit replica set dialog');
-    cy.get('[data-cy=meta-test__replicaSetSection] [data-cy="meta-test__editBtn"]').first().click();
-    cy.get('.meta-test__EditReplicasetModal input[name="all_rw"]').first().should('exist');
-    cy.get('.meta-test__EditReplicasetModal h2~svg').first().click(); // click close icon
-    cy.get('.meta-test__EditReplicasetModal').should('not.exist');
-  });
+    cy.get('.meta-test__ConfigureServerModal input[name="all_rw"]').should('exist');
 
-  it('Test: cartridge_hide_all_rw is TRUE', () => {
     ////////////////////////////////////////////////////////////////////
-    cy.log('Setup cartridge_hide_all_rw=true');
-    cy.task('tarantool', {
-      code: `
-      _G.cluster:server('dummy-1').env.TARANTOOL_CONSOLE_SOCK
-    `
-    }).then(([sock]) => {
-      expect(sock).to.be.a('string');
-      cy.task('tarantool', {
-        host: 'unix/', port: sock, code: `
-        local frontend = package.loaded['frontend-core']
-        frontend.set_variable('cartridge_hide_all_rw', true)
-        return true
-      `
-      }).should('deep.eq', [true]);
-    });
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open WebUI');
-    cy.visit('/admin/cluster/dashboard');
-    cy.title().should('eq', 'dummy-1: Cluster');
-    ////////////////////////////////////////////////////////////////////
-    cy.log('Open configure server dialog');
-    cy.get('.meta-test__UnconfiguredServerList .meta-test__configureBtn').first().click();
+    cy.log('cartridge_hide_all_rw is true');
+    hideAllRW('true')
+    cy.reload()
+
+    cy.get('.meta-test__ConfigureServerModal').contains('dummy-1');
     cy.get('.meta-test__ConfigureServerModal input[name="all_rw"]').should('not.exist');
 
-    cy.log('Create replica set');
-    cy.get('.meta-test__ConfigureServerModal form input[name="alias"]')
-      .should('be.focused')
-      .type('replica-name');
+    ////////////////////////////////////////////////////////////////////
+    cy.log('cartridge_hide_all_rw is false');
+    hideAllRW('false');
+    cy.reload()
 
-    cy.get('.meta-test__CreateReplicaSetBtn').click();
-    cy.get('.meta-test__ConfigureServerModal').should('not.exist');
-    cy.get('#root').contains('replica-name');
-    ////////////////////////////////////////////////////////////////
-    cy.log('Open edit replica set dialog');
-    cy.get('[data-cy=meta-test__replicaSetSection] [data-cy="meta-test__editBtn"]').first().click();
+    cy.get('.meta-test__ConfigureServerModal input[name="all_rw"]').should('exist');
+
+  });
+
+  it('Test: EditReplicasetForm', function () {
+    cy.task('tarantool', {code: '_G.cluster:bootstrap()'});
+
+    ////////////////////////////////////////////////////////////////////
+    cy.log('cartridge_hide_all_rw is unset');
+    hideAllRW('nil');
+    cy.visit(`/admin/cluster/dashboard?r=${this.main_server.replicaset_uuid}`);
+
+    cy.get('.meta-test__EditReplicasetModal input[name="all_rw"]').should('exist');
+
+    ////////////////////////////////////////////////////////////////////
+    cy.log('cartridge_hide_all_rw is true');
+    hideAllRW('true')
+    cy.reload()
+
+    cy.get('.meta-test__EditReplicasetModal').contains('dummy');
     cy.get('.meta-test__EditReplicasetModal input[name="all_rw"]').should('not.exist');
-    cy.get('.meta-test__EditReplicasetModal h2~svg').first().click(); // click close icon
-    cy.get('.meta-test__EditReplicasetModal').should('not.exist');
+
+    ////////////////////////////////////////////////////////////////////
+    cy.log('cartridge_hide_all_rw is false');
+    hideAllRW('false');
+    cy.reload()
+
+    cy.get('.meta-test__EditReplicasetModal input[name="all_rw"]').should('exist');
   });
 });
