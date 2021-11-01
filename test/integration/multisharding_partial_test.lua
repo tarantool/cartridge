48ab5aa9630d44832dcc98cea6627e23c1541444
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-11-01 04:16:46 +0300

    fix partial test

--- test/integration/multisharding_partial_test.lua
+++ test/integration/multisharding_partial_test.lua
@@ -1,20 +1,15 @@
 local fio = require('fio')
 local t = require('luatest')
 local g = t.group()
+local h = require('test.helper')
 
-local helpers = require('test.helper')
 
 g.before_each(function()
-    g.cluster = helpers.Cluster:new({
+    g.cluster = h.Cluster:new({
         datadir = fio.tempdir(),
-        server_command = helpers.entrypoint('srv_multisharding'),
-        cookie = 'secret',
+        server_command = h.entrypoint('srv_multisharding'),
+        cookie = h.random_cookie(),
         replicasets = {
-            {
-                alias = 'router',
-                roles = {'vshard-router'},
-                servers = 1
-            },
             {
                 alias = 'storage-hot',
                 roles = {'vshard-storage'},
@@ -24,75 +19,85 @@ g.before_each(function()
         },
     })
 
-    g.server = helpers.Server:new({
+    g.router = h.Server:new({
+        alias = 'router',
+        workdir = fio.pathjoin(g.cluster.datadir, 'router'),
+        command = h.entrypoint('srv_multisharding'),
+        replicaset_uuid = h.uuid('c'),
+        instance_uuid = h.uuid('c', 'c', 1),
+        http_port = 8084,
+        cluster_cookie = g.cluster.cookie,
+        advertise_port = 13304,
+    })
+
+    g.storage = h.Server:new({
         alias = 'storage-cold',
-        workdir = fio.pathjoin(g.cluster.datadir, 'spare'),
-        command = helpers.entrypoint('srv_multisharding'),
-        replicaset_uuid = helpers.uuid('d'),
-        instance_uuid = helpers.uuid('d', 'd', 1),
+        workdir = fio.pathjoin(g.cluster.datadir, 'storage-cold'),
+        command = h.entrypoint('srv_multisharding'),
+        replicaset_uuid = h.uuid('d'),
+        instance_uuid = h.uuid('d', 'd', 1),
         http_port = 8085,
-        vshard_group = nil,
         cluster_cookie = g.cluster.cookie,
         advertise_port = 13305,
-
     })
 
     g.cluster:start()
-    g.server:start()
+    g.storage:start()
+    g.router:start()
 end)
 
 g.after_each(function()
     g.cluster:stop()
-    g.server:stop()
+    g.storage:stop()
     fio.rmtree(g.cluster.datadir)
 end)
 
 g.test_bootstrap_vshard_by_group = function()
     local main = g.cluster.main_server
 
+    -- try to bootsrap vshard without router
+    t.assert_error_msg_contains('No remotes with role "vshard-router" available',
+        g.cluster.bootstrap_vshard, g.cluster)
+
+    -- join router
+    g.cluster:join_server(g.router)
+    g.router:setup_replicaset({
+        roles = {'vshard-router'},
+        uuid = g.router.replicaset_uuid,
+        alias = 'router',
+        master = {g.router.instance_uuid},
+    })
+
     -- bootstrap only hot group
     g.cluster:bootstrap_vshard()
 
-    t.assert_equals(helpers.list_cluster_issues(main), {{
+    t.assert_equals(h.list_cluster_issues(main), {{
         level = 'warning',
         topic = 'vshard',
         message = [[Group "cold" wasn't bootstrapped: Sharding config is empty. ]] ..
             [[Maybe you have no instances with such group?]],
-        instance_uuid = main.instance_uuid,
-        replicaset_uuid = main.replicaset_uuid,
+        instance_uuid = g.router.instance_uuid,
+        replicaset_uuid = g.router.replicaset_uuid,
     }})
 
+    -- try to bootstrap again
+    t.assert_error_msg_contains('already bootstrapped', g.cluster.bootstrap_vshard, g.cluster)
+
     -- join server with cold group
-    g.cluster:join_server(g.server)
-    g.server:setup_replicaset({
+    g.cluster:join_server(g.storage)
+    g.storage:setup_replicaset({
         roles = {'vshard-storage'},
         vshard_group = 'cold',
-        uuid = g.server.replicaset_uuid,
+        uuid = g.storage.replicaset_uuid,
         alias = 'storage-cold',
-        master = {g.server.instance_uuid},
+        master = {g.storage.instance_uuid},
         weight = 1,
     })
 
     -- bootstrap cold group
     g.cluster:bootstrap_vshard()
-    t.assert_equals(helpers.list_cluster_issues(main), {})
-end
+    t.assert_equals(h.list_cluster_issues(main), {})
 
-g.test_bootstrap_twice = function()
-    local main = g.cluster.main_server
-
-    -- bootstrap only hot group
-    g.cluster:bootstrap_vshard()
-
-    t.assert_equals(helpers.list_cluster_issues(main), {{
-        level = 'warning',
-        topic = 'vshard',
-        message = [[Group "cold" wasn't bootstrapped: Sharding config is empty. ]] ..
-            [[Maybe you have no instances with such group?]],
-        instance_uuid = main.instance_uuid,
-        replicaset_uuid = main.replicaset_uuid,
-    }})
-
-    -- try again
+    -- try to bootstrap again
     t.assert_error_msg_contains('already bootstrapped', g.cluster.bootstrap_vshard, g.cluster)
 end
