423184c972e1f9c5717b4d9bba3777270cdba09b
Igor Zolotarev <63460867+yngvar-antonsson@users.noreply.github.com>
2022-01-12 14:56:16 +0300

    Add retrying to vshard test (#1696)

--- test/integration/vshard_test.lua
+++ test/integration/vshard_test.lua
@@ -124,21 +124,22 @@ g.test_bucket_ref_on_replica_prevent_bucket_move = function()
     })
 
     -- send bucket to another storage
-    local bucket_counts = g.cluster:server('storage-1'):exec(function(bucket_id, replicaset_uuid)
+    g.cluster:server('storage-1'):exec(function(bucket_id, replicaset_uuid)
         assert(not box.info.ro)
         local vshard_storage = require('vshard.storage')
 
         vshard_storage.bucket_send(bucket_id, replicaset_uuid)
-
-        -- yield to send data
-        require('fiber').yield()
-
-        return box.space.test:pairs():filter(function(x)
-            return x.bucket_id == bucket_id
-        end):length()
     end, {some_bucket_id, single_replicaset_uuid})
 
-    t.assert_not_equals(bucket_counts, 0)
+    h.retrying({}, function()
+        local bucket_counts = g.cluster:server('storage-1'):exec(function(bucket_id)
+            return box.space.test:pairs():filter(function(x)
+                return x.bucket_id == bucket_id
+            end):length()
+        end, {some_bucket_id})
+
+        t.assert_not_equals(bucket_counts, 0)
+    end)
 
     -- unref bucket on replica
     g.cluster:server('storage-2'):exec(function(some_bucket_id)
