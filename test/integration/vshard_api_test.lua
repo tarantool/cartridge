7f7c6bd0b09602723bb9ec526c7a891db7c4e309
Andrei Sidorov <andrei.sidorov@vk.team>
2022-03-01 15:27:33 +0300

    add sharding in tests

--- test/integration/vshard_api_test.lua
+++ test/integration/vshard_api_test.lua
@@ -8,14 +8,14 @@ g.before_test('test_get_cfg', function()
     g.cluster = helpers.Cluster:new({
         datadir = fio.tempdir(),
         server_command = helpers.entrypoint('srv_basic'),
-        cookie = helpers.random_cookie(),
+        cookie = 'qwerty123',
         replicasets = { {
                 alias = 'A',
                 roles = {'vshard-router'},
                 servers = 1,
             }, {
                 alias = 'B',
-                roles = {'myrole'},
+                roles = {'vshard-storage'},
                 servers = 2,
             },
         }
@@ -42,7 +42,23 @@ function g.test_get_cfg()
             rebalancer_max_sending = 1,
             sched_move_quota = 1,
             sched_ref_quota = 300,
-            sharding = {},
+            sharding = {
+                [g.cluster.replicasets[2].uuid] = {
+                    replicas = {
+                        [g.B1.instance_uuid] = {
+                            master = true,
+                            name = "localhost:13302",
+                            uri = "admin:qwerty123@localhost:13302",
+                        },
+                        [g.B2.instance_uuid] = {
+                            master = false,
+                            name = "localhost:13303",
+                            uri = "admin:qwerty123@localhost:13303",
+                        },
+                    },
+                    weight = 1,
+                },
+            },
             sync_timeout = 1,
         },
     }
@@ -63,16 +79,30 @@ g.before_test('test_get_cfg_multisharding', function()
     g.cluster = helpers.Cluster:new({
         datadir = fio.tempdir(),
         server_command = helpers.entrypoint('srv_multisharding'),
-        cookie = helpers.random_cookie(),
-        replicasets = { {
-                alias = 'R',
+        cookie = 'ytrewq321',
+        use_vshard = true,
+        replicasets = {
+            {
+                alias = 'router',
                 roles = {'vshard-router'},
                 servers = 1,
+            }, {
+                alias = 'storage-hot',
+                roles = {'vshard-storage'},
+                vshard_group = 'hot',
+                servers = 1,
+            }, {
+                alias = 'storage-cold',
+                roles = {'vshard-storage'},
+                vshard_group = 'cold',
+                servers = 1,
             },
-        }
+        },
     })
     g.cluster:start()
-    g.R1 = assert(g.cluster:server('R-1'))
+    g.router = assert(g.cluster:server('router-1'))
+    g.storage_hot = assert(g.cluster:server('storage-hot-1'))
+    g.storage_cold = assert(g.cluster:server('storage-cold-1'))
 end)
 
 g.after_test('test_get_cfg_multisharding', function()
@@ -82,18 +112,6 @@ end)
 
 function g.test_get_cfg_multisharding()
     local expected_cfg = {
-        cold = {
-            bucket_count = 2000,
-            collect_lua_garbage = false,
-            read_only = false,
-            rebalancer_disbalance_threshold = 1,
-            rebalancer_max_receiving = 100,
-            rebalancer_max_sending = 1,
-            sched_move_quota = 1,
-            sched_ref_quota = 300,
-            sharding = {},
-            sync_timeout = 1,
-        },
         hot = {
             bucket_count = 30000,
             collect_lua_garbage = false,
@@ -103,11 +121,51 @@ function g.test_get_cfg_multisharding()
             rebalancer_max_sending = 1,
             sched_move_quota = 1,
             sched_ref_quota = 300,
-            sharding = {},
+            sharding = {
+                [g.cluster.replicasets[2].uuid] = {
+                    replicas = {
+                        [g.storage_hot.instance_uuid] = {
+                            master = true,
+                            name = "localhost:13302",
+                            uri = "admin:ytrewq321@localhost:13302",
+                        },
+                    },
+                    weight = 1,
+                },
+            },
+            sync_timeout = 1,
+        },
+        cold = {
+            bucket_count = 2000,
+            collect_lua_garbage = false,
+            read_only = false,
+            rebalancer_disbalance_threshold = 1,
+            rebalancer_max_receiving = 100,
+            rebalancer_max_sending = 1,
+            sched_move_quota = 1,
+            sched_ref_quota = 300,
+            sharding = {
+                [g.cluster.replicasets[3].uuid] = {
+                    replicas = {
+                        [g.storage_cold.instance_uuid] = {
+                            master = true,
+                            name = "localhost:13303",
+                            uri = "admin:ytrewq321@localhost:13303",
+                        },
+                    },
+                    weight = 1,
+                },
+            },
             sync_timeout = 1,
         },
     }
 
-    local cfg = g.R1:call('cartridge_vshard_get_config', {})
+    local cfg = g.router:call('cartridge_vshard_get_config', {})
+    t.assert_equals(cfg, expected_cfg)
+
+    local cfg = g.storage_hot:call('cartridge_vshard_get_config', {})
+    t.assert_equals(cfg, expected_cfg)
+
+    local cfg = g.storage_cold:call('cartridge_vshard_get_config', {})
     t.assert_equals(cfg, expected_cfg)
 end
