b6b9ee2fd8a358fd5b73a9936cf302bcc70dbc0a
Alexsandr Litkevich <alexshow2362@gmail.com>
2022-07-14 13:35:47 +0400

    Added filling in fields on reboot

--- webui/src/models/cluster/server-list/gate.ts
+++ webui/src/models/cluster/server-list/gate.ts
@@ -3,8 +3,6 @@ import { createGate } from 'effector-react';
 export type FilterGateType = {
   rolesFilter: string;
   ratingFilter: string;
-  // Record: () => void;
-  // Read: () => void;
 };
 
 export const FilterGate = createGate<FilterGateType>({});
