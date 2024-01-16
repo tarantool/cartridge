b469d0861882bc45bb1d0a68599eeefee18b6320
Igor Zolotarev <63460867+yngvar-antonsson@users.noreply.github.com>
2024-01-16 16:07:54 +0300

    Update vshard to 0.1.25 (#2172)
    * Update vshard to 0.1.25
    
    * tests: fix vshard error check
    
    Since vshard is compatible with Tarantool 3.0 it supports
    string replicaset identifiers so they renamed replicaset_uuid to
    replicaset.
    
    * add "vshard.conn_man" fiber to ignore
    
    This fiber runs infinite loop and seems to have reaction to only
    module reload.
    
    * fix doc build
    
    Seems before this patch some dependencies broke rst build.
    This patch should fix it.
    
    See https://github.com/apache/flink/pull/24086
    
    ---------
    
    Co-authored-by: Oleg Babin <babinoleg@mail.ru>

--- test/integration/rebalancing_test.lua
+++ test/integration/rebalancing_test.lua
@@ -159,7 +159,7 @@ function g.test()
     t.assert_covers(err, {
         type = 'ShardingError',
         name = 'MISSING_MASTER',
-        replicaset_uuid = g.sA1.replicaset_uuid,
+        replicaset = g.sA1.replicaset_uuid,
         message = 'Master is not configured for' ..
             ' replicaset ' .. g.sA1.replicaset_uuid,
     })
@@ -168,7 +168,7 @@ function g.test()
 
     helpers.retrying({}, function() g.sA1:eval([[
         for _, f in pairs(require('fiber').info()) do
-            if f.name:startswith('vshard.') then
+            if f.name:startswith('vshard.') and f.name ~= 'vshard.conn_man' then
                 error('Fiber ' .. f.name .. ' still alive', 0)
             end
         end
